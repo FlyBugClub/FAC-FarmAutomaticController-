da58d4cc16339efd3e9538deacde60e325292b44
NQD2308 <nqd230899@gmail.com>
2024-03-10 22:50:40 +0700

    Update bnt_Data

--- FAC_FarmAutomaticController/App.js
+++ FAC_FarmAutomaticController/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <Button
+        title='Get Data'
+        color="#841584"/>
+      <Button
+        title='Send Data'
+        color="#841584"/>
       <StatusBar style="auto" />
     </View>
   );
