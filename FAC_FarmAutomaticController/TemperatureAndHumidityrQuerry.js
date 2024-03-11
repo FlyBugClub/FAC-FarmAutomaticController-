e268c04da349bce9950600c1cf0d8e9a005ffda9
KhoiNguyen <khoinm0603@gmail.com>
2024-03-11 20:05:34 +0700

    rename HistoryCheck.js to TemperatureAndHumidityQuerry.js

---
+++ FAC_FarmAutomaticController/TemperatureAndHumidityrQuerry.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+const sql = require('mssql');
+
+class HistoryCheck {
+    constructor(config) {
+        config = this.config;
+    }
+
+}
+
+const che
+
+export default HistoryCheck;
