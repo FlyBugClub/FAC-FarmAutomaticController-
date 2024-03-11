e18a851f23f0ff702f6453546061605a8b6cdb6f
Đinh Quốc Cường <tr6r20@gmail.com>
2024-03-11 10:46:13 +0700

    update mqtt platform

--- FAC_FarmAutomaticController/App.js
+++ FAC_FarmAutomaticController/App.js
@@ -1,58 +1,195 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity, Alert, TextInput } from 'react-native';
+import Slider from '@react-native-community/slider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import init from 'react_native_mqtt';
+
+init({
+  size: 10000,
+  storageBackend: AsyncStorage,
+  defaultExpires: 1000 * 3600 * 24,
+  enableCache: true,
+  sync : {}
+});
+const options = {
+  host: 'broker.emqx.io',
+  port: 8083,
+  path: '/mqtt',
+  id: 'id_' + parseInt(Math.random()*100000)
+};
+
+client = new Paho.MQTT.Client(options.host, options.port, options.path);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topic: "tr6r/cuong",
+      message: '',
+      messageList: [],
+      sliderValue: 0,
+      statusAuto: false,
+      statusManual: false
+    };
+    client.onConnectionLost = this.onConnectionLost;
+    client.onMessageArrived = this.onMessageArrived;
+  }
+  onConnect = () => {
+    console.log('onConnect');
+
+  }
+
+  onFailure = (err) => {
+    console.log('Connect failed!');
+ 
+  }
+
+  connect = () => {
+
+        client.connect({
+          onSuccess: this.onConnect,
+          useSSL: false,
+          timeout: 3,
+          onFailure: this.onFailure
+        });
+
+  }
+
+  subscribeTopic = () => {
+
+        
+        client.subscribe(this.state.topic, { qos: 0 });
+      }
+  
+  
+
+  sendMessage = () =>{
+    var message = new Paho.MQTT.Message("cuong : hehelo");
+    message.destinationName = this.state.topic;
+    client.send(message);
+  }
+  onConnectionLost=(responseObject)=>{
+    this.connect();
+  }
+
+  onMessageArrived = (message )=> {
+   
+    console.log('onMessageArrived:' + JSON.parse(message.payloadString));
+   
+  }
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.sensor_value}>
-        <SensorTempText title1={'26°C'} title2={'Temp'}></SensorTempText>
-        <SensorTempText title1={'46.6%'} title2={'Humadity'}></SensorTempText>
-        <SensorTempText title1={'30'} title2={'Air'}></SensorTempText>
+  handleSliderChange = (value) => {
+    
+    this.setState({ sliderValue: value });
+  };
+
+  handleSliderComplete = (value) => {
+    // Khi người dùng kết thúc việc điều chỉnh slider, bạn có thể lấy giá trị ở đây
+    this.setState({ sliderValue: value });
+    console.log('Slider value after complete:', value);
+  };
+
+  pressauto =  () => {
+    
+      console.log("hehe");
+      this.subscribeTopic();
+  };
+
+  pressmanual =  () => {
+    
+    console.log("haha");
+    this.sendMessage();
+};
+
+  render() {
+    const { status, messageList,sliderValue  } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={styles.sensor_value}>
+          <SensorTempText title1={'26°C'} title2={'Temp'}></SensorTempText>
+          <SensorTempText title1={'46.6%'} title2={'Humidity'}></SensorTempText>
+          <SensorTempText title1={'30'} title2={'Air'}></SensorTempText>
+        </View>
+
+        <Slider 
+                  style={{width: 200, height: 40}}
+                  minimumValue={50}
+                  maximumValue={95}
+                  value={sliderValue}
+                  onValueChange={this.handleSliderChange}
+                  onSlidingComplete={this.handleSliderComplete}
+        />
+        <Text style={styles.sliderValueText}>Slider Value: {Math.round(sliderValue)}</Text>
+
+        <View style={{ flexDirection: 'row' }}>
+          <CustomButton
+             type='solid'
+             title='CONNECT'
+             onPress={this.connect}
+             buttonStyle={{
+              marginBottom:50,
+              backgroundColor: status === 'failed' ? 'red' : '#397af8'
+            }}
+            icon={{ name: 'lan-connect', type: 'material-community', color: 'white' }}
+            loading={status === 'isFetching' ? true : false}
+            disabled={status === 'isFetching' ? true : false}
+          />
+          <CustomButton
+            title={'Auto'}
+          
+            onPress={this.pressauto}
+          
+          />
+
+          <CustomButton
+            title={'Manual'}
+          
+            onPress={this.pressmanual}
+          
+          />
+        </View>
+
+        <StatusBar style="auto" />
       </View>
-
-      <TextInput
-      style={styles.TextValue}
-      placeholder='Value...'/>
-
-      <View style={{flexDirection: 'row'}}>
-        <CustomButton title={'Get Data'} onPress={() => Alert.alert('Get Data Already')}/>
-        <CustomButton title={'Send Data'} onPress={() => Alert.alert('Send Data Already')}/>
-      </View>
-      
-      <StatusBar style="auto"/>
-    </View>
-  );
+    );
+  }
 }
 
-const SensorTempText = ({title1, title2}) => (
-  <View>
-    <Text style={[styles.TextInSensorArea, {color: '#4d587d'}, {fontSize: 18}]}>{title1}</Text>
-    <Text style={[styles.TextInSensorArea, {color: '#c1c1c1'}]} >{title2}</Text>
-  </View>
-);
+class SensorTempText extends Component {
+  render() {
+    const { title1, title2 } = this.props;
+    return (
+      <View>
+        <Text style={[styles.TextInSensorArea, { color: '#4d587d' }, { fontSize: 18 }]}>{title1}</Text>
+        <Text style={[styles.TextInSensorArea, { color: '#c1c1c1' }]}>{title2}</Text>
+      </View>
+    );
+  }
+}
 
-const CustomButton = ({ onPress, title }) => {
-  const [isPressed, setIsPressed] = useState(false);
-
-  return (
-    <TouchableOpacity
-      style={[
-        styles.button,
-        { backgroundColor: isPressed ? '#2D314A' : 'white' }
-      ]}
-      onPressIn={() => setIsPressed(true)}
-      onPressOut={() => setIsPressed(false)}
-      onPress={onPress}
-    >
-      <Text style={[
-        styles.btn_data, 
-        { fontWeight: 'bold' }, 
-        { color: isPressed ? 'white' : '#2D314A' }]}>{title}</Text>
-    </TouchableOpacity>
-  );
-};
+class CustomButton extends Component {
+  render() {
+    const { onPress, title, isPressed, onPressIn, onPressOut } = this.props;
+    return (
+      <TouchableOpacity
+        style={[
+          styles.button,
+          { backgroundColor: isPressed ? '#2D314A' : 'white' }
+        ]}
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}
+        onPress={onPress}
+      >
+        <Text style={[
+          styles.btn_data,
+          { fontWeight: 'bold' },
+          { color: isPressed ? 'white' : '#2D314A' }]}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -66,19 +203,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 50,
-    // borderColor: 'black',
-    // borderWidth: 1,
-    // padding: 5
   },
   TextValue: {
-    background: '#fff',
+    backgroundColor: '#fff',
     borderColor: 'black',
     borderWidth: 1,
     padding: 5,
     borderRadius: 8,
     marginBottom: 30,
   },
-  TextInSensorArea:{
+  TextInSensorArea: {
     fontWeight: 'bold',
     textAlign: 'center',
   },
@@ -93,3 +227,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
+
+export default App;
