11035328df34fabf4634ba9297e824ecb5c8ea87
duc <taduc1130@gmail.com>
2024-03-13 23:51:39 +0700

    add template MQTT

--- MushroomAutomation/MushroomAutomation.ino
+++ MushroomAutomation/MushroomAutomation.ino
@@ -3,6 +3,16 @@
 #include <Wire.h>
 #include "Adafruit_SHT31.h"
 
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+// Thông tin về MQTT Broker
+#define mqtt_server "broker.emqx.io"
+const uint16_t mqtt_port = 1883; //Port của MQTT broker
+#define mqtt_topic_temp "temperature"
+#define mqtt_topic_humi "humidity"
+PubSubClient client(espClient);
+StaticJsonDocument<256> doc;
+
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 #include "secret_pass.h"
@@ -41,6 +51,9 @@ void setup() {
     Serial.println("DISABLED");
     // Đọc giá trị từ chân V6 khi chương trình chạy lần đầu
   Blynk.syncVirtual(V6);
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  reconnect();
 }
 
 void autoControlMode(float& temperature, float& humidity) {
@@ -77,6 +90,11 @@ void loop() {
   Blynk.setProperty(V6, "color", autoControl ? "#2EA5D8" : "#FF0000");
   Serial.println("desiredhumidity");
   Serial.println(desiredHumidity);
+  if (!client.connected()) {
+    reconnect();
+  }
+  
+  client.loop();
   if (!autoControl) {
     BLYNK_WRITE(V3);
   }
@@ -124,4 +142,82 @@ BLYNK_WRITE(V5) {
 
 BLYNK_WRITE(V6) {
   desiredHumidity = param.asFloat();
+}
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.println(topic);
+  String checkLed;
+if(String(topic) == mqtt_topic_humi){
+  // Parse the JSON message
+  StaticJsonDocument<200> jsonDocument;
+  deserializeJson(jsonDocument, payload, length);
+  const String ledStatus = jsonDocument["statusLed"];
+  checkLed = ledStatus;
+  Serial.println("Message received: ");
+  Serial.print("StatusLed: ");
+  Serial.println(ledStatus);
+  
+   if (ledStatus == "on") {
+    digitalWrite(led, HIGH);
+  } else if (ledStatus == "off")  {
+    digitalWrite(led, LOW);
+  }
+}
+
+if(String(topic) == mqtt_topic_temp){
+  // Parse the JSON message
+  StaticJsonDocument<200> jsonDocument;
+  deserializeJson(jsonDocument, payload, length);
+  int gasStatus = jsonDocument["statusGas"];
+  Serial.println("Message received: ");
+  Serial.print("statusGas: ");
+  Serial.println(gasStatus);
+  
+   if (gasStatus > 320) {
+    digitalWrite(led, LOW);
+  } else if (gasStatus < 320 && checkLed == "on") {
+    digitalWrite(led, HIGH);
+  }
+  else if(gasStatus < 320 && checkLed == "off"){
+    digitalWrite(led, LOW);
+  }
+}
+}
+void publishLedData(String ledStatus ) {
+  char buffer[256];
+  size_t n = serializeJson(doc, buffer);
+  
+ 
+  doc["statusLed"] = ledStatus;
+  
+  n = serializeJson(doc, buffer);
+  client.publish(mqtt_topic_led, buffer, n);
+  
+}
+void publishGasData(int gasStatus) {
+  char buffer[256];
+  size_t n = serializeJson(doc, buffer);
+  
+  
+  doc["statusGas"] = gasStatus;
+  
+  n = serializeJson(doc, buffer);
+  client.publish(mqtt_topic_gas, buffer, n);
+  
+}
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("helloem")) {
+      // Đăng kí nhận gói tin tại topic wemos/ledstatus
+      client.subscribe(mqtt_topic_humi);
+      client.subscribe(mqtt_topic_temp);
+      
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
 }
