acd3f4c3bef58b9872f10bd2db8aefc4ecc932cb
duc <taduc1130@gmail.com>
2024-03-14 16:48:30 +0700

    send hello to mqttx

--- MushroomAutomation/MushroomAutomation.ino
+++ MushroomAutomation/MushroomAutomation.ino
@@ -2,37 +2,23 @@
 #include <Wire.h>
 #include "Adafruit_SHT31.h"
 
-#include <ESP8266WiFi.h> // Include the ESP8266WiFi library
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-// Thông tin về MQTT Broker
-#define mqtt_server "broker.emqx.io"
 #include "secret_pass.h"
 
-const uint16_t mqtt_port = 1883; //Port của MQTT broker
-#define mqtt_topic_temp "temperature"
-#define mqtt_topic_humi "humidity"
-WiFiClient espClient; // Now WiFiClient should be recognized
+const char* mqtt_server = "broker.emqx.io";
+const uint16_t mqtt_port = 1883;
+const char* mqtt_topic_hello = "hello_topic";
+
+WiFiClient espClient;
 PubSubClient client(espClient);
-StaticJsonDocument<256> doc;
-
-Adafruit_SHT31 sht31 = Adafruit_SHT31();
-
-const int pumpPin = D6; // Chân kết nối của máy bơm với ESP8266
-
-bool autoControl = true;
-float desiredTemperature = 25.0;
-float desiredHumidity = 60.0;
-
-unsigned long lastSprayTime = 0;
 
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("helloem")) {
-      client.subscribe(mqtt_topic_humi);
-      client.subscribe(mqtt_topic_temp);
-      
+      Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -43,77 +29,35 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(pumpPin, OUTPUT);
   Serial.begin(9600);
-  
-  while (!Serial)
-    delay(10);
 
-  Serial.println("SHT31 test");
-  if (!sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-    Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
+  // Initialize WiFi connection
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println("");
+  Serial.println("WiFi connected");
 
+  // Initialize MQTT connection
   client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
   reconnect();
-}
-
-void autoControlMode(float& temperature, float& humidity) {
-  float t = sht31.readTemperature();
-  float currentHumidity = sht31.readHumidity();
-
-  unsigned long currentMillis = millis();
-  
-  if (currentHumidity < humidity) {
-    if (currentMillis - lastSprayTime >= 10000) {
-      digitalWrite(pumpPin, HIGH);
-      lastSprayTime = currentMillis; 
-      delay(2000);
-      digitalWrite(pumpPin, LOW);
-    }
-  } else {
-    digitalWrite(pumpPin, LOW);
-  }
 }
 
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  
   client.loop();
 
-  if (autoControl) {
-    autoControlMode(desiredTemperature, desiredHumidity);
+  // Send "hello" message
+  sendHelloMessage();
+  delay(1000); // Adjust delay as needed
+}
+
+void sendHelloMessage() {
+  if (client.connected()) {
+    client.publish(mqtt_topic_hello, "hello");
   }
-
-  float temperature = sht31.readTemperature();
-  float humidity = sht31.readHumidity();
-
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.print("% - Temperature: ");
-  Serial.print(temperature);
-  Serial.println("°C");
-
-  publishData(temperature, humidity);
-  delay(2000); // Đợi 2 giây trước khi đọc lại dữ liệu từ cảm biến
 }
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived on topic: ");
-  Serial.println(topic);
-}
-
-void publishData(float temperature, float humidity) {
-  doc["temperature"] = temperature;
-  doc["humidity"] = humidity;
-
-  char buffer[256];
-  size_t n = serializeJson(doc, buffer);
-  
-  client.publish(mqtt_topic_temp, buffer, n);
-}
-
---
+++ MushroomAutomation/secret_pass.h
@@ -0,0 +1,25 @@
+//https://github.com/Risele/SHT3x library
+char ssid[] = "Basic Coffee";
+char pass[] = "";
+
+// char ssid[] = "Bu Bill";
+// char pass[] = "@laphoa2017";
+
+// nha thay P
+// #define BLYNK_TEMPLATE_ID      "TMPL6r0fdPEst"
+// #define BLYNK_TEMPLATE_NAME    "Mushroom"
+// #define BLYNK_AUTH_TOKEN       "zBHRmPs-55II61f8lNHAkv9H3Q4ACr0C"
+
+// nha Duc
+#define BLYNK_TEMPLATE_ID "TMPL6r0fdPEst"
+#define BLYNK_TEMPLATE_NAME "Mushroom"
+#define BLYNK_AUTH_TOKEN "2Phztp6f2upmVuVaX98VSv9u_nwjjvM2"
+/*
+char ssid[] = "";
+char pass[] = "";
+
+
+#define BLYNK_TEMPLATE_ID      ""
+#define BLYNK_TEMPLATE_NAME    ""
+#define BLYNK_AUTH_TOKEN       ""
+*/
