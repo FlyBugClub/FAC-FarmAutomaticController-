e128bc7b472893bd36d6da357349dee813023c19
duc <taduc1130@gmail.com>
2024-03-14 22:56:00 +0700

    update code

--- MushroomAutomation/MushroomAutomation.ino
+++ MushroomAutomation/MushroomAutomation.ino
@@ -10,6 +10,7 @@
 const char* mqtt_server = "broker.emqx.io";
 const uint16_t mqtt_port = 1883;
 const char* mqtt_topic_hello = "hello_topic";
+const char* mqtt_topic_hi = "hi_topic";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -42,6 +43,7 @@ void setup() {
 
   // Initialize MQTT connection
   client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
   reconnect();
 }
 
@@ -55,9 +57,63 @@ void loop() {
   sendHelloMessage();
   delay(1000); // Adjust delay as needed
 }
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.println(topic);
+
+  // Check if the received message is from the "hello" topic
+  if (strcmp(topic, mqtt_topic_hello) == 0) {
+    // Convert payload to string
+    String payloadStr;
+    for (int i = 0; i < length; i++) {
+      payloadStr += (char)payload[i];
+    }
+    
+    // Parse the JSON payload
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, payloadStr);
+
+    // Check for parsing errors
+    if (error) {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.f_str());
+      return;
+    }
+
+    // Extract value based on key
+    const char* msg = doc["msg"];
+
+    // Print the extracted value
+    Serial.print("Received message: ");
+    Serial.println(msg);
+    
+    // Send "hi" message in response
+    sendHiMessage();
+  }
+}
+
 
 void sendHelloMessage() {
+  StaticJsonDocument<200> doc;
+  doc["msg"] = "hello";
+  
+  char buffer[256];
+  size_t n = serializeJson(doc, buffer);
+
   if (client.connected()) {
-    client.publish(mqtt_topic_hello, "hello");
+    client.publish(mqtt_topic_hello, buffer, n);
+  }
+}
+
+
+void sendHiMessage() {
+  StaticJsonDocument<200> doc;
+  doc["msg"] = "hi";
+  
+  char buffer[256];
+  size_t n = serializeJson(doc, buffer);
+
+  if (client.connected()) {
+    client.publish(mqtt_topic_hi, buffer, n);
   }
 }
