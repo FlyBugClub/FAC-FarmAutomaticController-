7e55aa8a0f863c2c67c7a0b0e344e9ba6ef8cdb4
Đinh Quốc Cường <tr6r20@gmail.com>
2024-03-21 17:13:47 +0700

    update get data for foreach esp in home views

--- FAC_FarmAutomaticController/Views/Home.js
+++ FAC_FarmAutomaticController/Views/Home.js
@@ -8,7 +8,7 @@ import { Icon } from '@rneui/base';
 import MyContext from '../DataContext.js';
 import { index } from 'd3-array';
 
-
+const data = [];
 export default class Home extends Component {
     LoginPage = () => {
         console.log("Login Page");
@@ -18,32 +18,53 @@ export default class Home extends Component {
         console.log("SignUp Page");
         this.props.navigation.navigate('SignUp'); 
     };
-    DetailPage = () => {
-        this.handleUpdateMessage();
-        this.props.navigation.navigate('Details'); 
+    DetailPage = (index) => {
+        console.log(index)
+        this.GetEquidmentValues(index);
+        // this.props.navigation.navigate('Details'); 
     };
-
+    
 
     static contextType = MyContext;
-
-    handleUpdateMessage = () => {
+    GetEquidmentValues = async  (index) => {
+        try {
       // Lấy giá trị context và cập nhật dữ liệu trong MyContext từ Home component
       const { addDataAtIndex } = this.context;
+      console.log('http://127.0.0.1:5000/api/get_equipmentlastinfo/'+data[index])
+      const response = await fetch('http://127.0.0.1:5000/api/get_equipmentlastinfo/'+data[index]);
+    //   if (!response.ok) {
+    //       throw new Error('Failed to fetch data');
+    //   }
+
+    console.log("haha");
+
+      const data_equidmentvalues = await response.json();
+    //   Tiếp tục xử lý dữ liệu nhận được từ API
+      console.log("hêh");
       addDataAtIndex({ id_esp1: 'ESP0001',id_esp2:"ESP002" },1);
+        }catch (error) {
+            console.error('Error fetching data:', error);
+        }
     };
 
-
     render() {
+        const { dataArray } = this.context;
+        // const jsonObject = JSON.parse(dataArray[1]);
+        const keyCount = Object.keys(dataArray[1]).length;
+        const handleDetailPress = (index) => {
+            this.DetailPage(index);
+        };
         const farmHouseList = [];
         // Sử dụng forEach để thêm các phần tử vào mảng items
-        [...Array(10)].forEach((_, index) => {
+        [...Array(keyCount)].forEach((_, index) => {
+            data[index] = Object.values(dataArray[1])[index];
             farmHouseList.push(
                 <View>
-                    <TouchableOpacity style={styles.famrItem} onPress={this.DetailPage}>
+                    <TouchableOpacity style={styles.famrItem} onPress={() => handleDetailPress(index)}>
                         <View style={{flexDirection: 'row', alignItems: 'center'}}>
                             <Ionicons name="water-outline" size={50} color={'#333'}/>
                             <View>
-                                <Text style={styles.titleItem} numberOfLines={1}> Farm {index}: Nấm Bào Ngư hải sản</Text>
+                                <Text style={styles.titleItem} numberOfLines={1}> Farm {index}: {Object.values(dataArray[1])[index]}</Text>
                                 <Text style={{ color: '#333', marginTop: 5 }}> 75% - 86%</Text>
                             </View>
                         </View>
