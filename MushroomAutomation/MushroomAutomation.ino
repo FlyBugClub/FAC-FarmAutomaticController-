2c8c2eec4a199dc84625b3b1b9530666c9f0f006
duc <taduc1130@gmail.com>
2024-04-02 23:48:46 +0700

    send and get data from api

--- MushroomAutomation/MushroomAutomation.ino
+++ MushroomAutomation/MushroomAutomation.ino
@@ -3,8 +3,10 @@
 #include "Adafruit_SHT31.h"
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
 
-const char* serverUrl = "http://example.com/api/data";
+const char* serverUrl = "https://ngunemay123.bsite.net/api/login/admin@gmail.com/123456";
 const int pumpPin = D6;
 bool autoControl = true;
 float desiredTemperature = 25.0;
@@ -41,13 +43,23 @@ void loop() {
     connectToWiFi();
   }
 
-  if (!sensorConnected) {
-    reconnectSensor();
-  } else {
-    float temperature = sht31.readTemperature();
-    float humidity = sht31.readHumidity();
-    manageAutoControl();
-  }
+  // if (!sensorConnected) {
+  //   reconnectSensor();
+  // } else {
+  //   float temperature = sht31.readTemperature();
+  //   float humidity = sht31.readHumidity();
+  //   manageAutoControl();
+  // }
+
+   String jsonString;
+    if (getDataFromAPI(serverUrl, jsonString)) {
+      Serial.println("Received JSON data:");
+      Serial.println(jsonString);
+    } else {
+      Serial.println("Failed to get data from API");
+    }
+  
+  
 }
 
 void connectToWiFi() {
@@ -58,6 +70,7 @@ void connectToWiFi() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
     if (millis() - startTime > 30000) {
+      Serial.println("WiFi connection timeout!");
       break;
     }
   }
@@ -66,6 +79,8 @@ void connectToWiFi() {
     wifiConnected = true;
     Serial.println("WiFi connected");
     autoControl = true;
+
+    // Gọi hàm sendDataToAPI() ở đây nếu bạn muốn gửi dữ liệu ngay sau khi kết nối WiFi thành công
   } else {
     wifiConnected = false;
     Serial.println("WiFi connection failed");
@@ -92,4 +107,52 @@ void autoControlMode(float& temperature, float& humidity) {
   } else {
     digitalWrite(pumpPin, LOW);
   }
+}
+
+void sendDataToAPI(float temperature, float humidity) {
+  HTTPClient http;
+  WiFiClient client; // Tạo một đối tượng WiFiClient
+  
+  StaticJsonDocument<200> jsonBuffer;
+  JsonObject jsonData = jsonBuffer.to<JsonObject>();
+
+  jsonData["temperature"] = temperature;
+  jsonData["humidity"] = humidity;
+
+  String jsonString;
+  serializeJson(jsonData, jsonString);
+
+  http.begin(client, serverUrl);
+  http.addHeader("Content-Type", "application/json");
+
+  int httpResponseCode = http.POST(jsonString);
+
+  if (httpResponseCode == 200) {
+    Serial.println("Data sent successfully");
+  } else {
+    Serial.print("Error sending data. HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+}
+
+bool getDataFromAPI(const char* url, String& jsonString) {
+  HTTPClient http;
+  WiFiClient client; // Tạo một đối tượng WiFiClient
+
+  // Send HTTP GET request
+  http.begin(client, serverUrl, 443, "/path", true);
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode == HTTP_CODE_OK) {
+    jsonString = http.getString();
+    return true;
+  } else {
+    Serial.print("HTTP request failed with error code: ");
+    Serial.println(httpResponseCode);
+    return false;
+  }
+
+  http.end();
 }
--- MushroomAutomation/secret_pass.h
+++ MushroomAutomation/secret_pass.h
@@ -1,6 +1,6 @@
 //https://github.com/Risele/SHT3x library
-char ssid[] = "Basic Coffee";
-char pass[] = "";
+// char ssid[] = "Basic Coffee";
+// char pass[] = "";
 
 // char ssid[] = "Bu Bill";
 // char pass[] = "@laphoa2017";
@@ -11,9 +11,12 @@ char pass[] = "";
 // #define BLYNK_AUTH_TOKEN       "zBHRmPs-55II61f8lNHAkv9H3Q4ACr0C"
 
 // nha Duc
-#define BLYNK_TEMPLATE_ID "TMPL6r0fdPEst"
-#define BLYNK_TEMPLATE_NAME "Mushroom"
-#define BLYNK_AUTH_TOKEN "2Phztp6f2upmVuVaX98VSv9u_nwjjvM2"
+// #define BLYNK_TEMPLATE_ID "TMPL6r0fdPEst"
+// #define BLYNK_TEMPLATE_NAME "Mushroom"
+// #define BLYNK_AUTH_TOKEN "2Phztp6f2upmVuVaX98VSv9u_nwjjvM2"
+
+char ssid[] = "Thu Tamm";
+char pass[] = "thutam1975";
 /*
 char ssid[] = "";
 char pass[] = "";
