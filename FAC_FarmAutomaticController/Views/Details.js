f0d32d631f6efb10516df83ff33cf7fb7f588482
NQD2308 <nqd230899@gmail.com>
2024-04-03 21:03:50 +0700

    update home & detail

--- FAC_FarmAutomaticController/Views/Details.js
+++ FAC_FarmAutomaticController/Views/Details.js
@@ -20,6 +20,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import Slider from "@react-native-community/slider";
 import { LineChart } from "react-native-chart-kit";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
 import MyContext from "../DataContext.js";
 import apiUrl from "../apiURL.js";
 import * as Notifications from "expo-notifications";
@@ -84,6 +85,18 @@ export default class Details extends Component {
       //DateTime
       dateTime: new Date(),
       showPicker: false,
+
+      //Picker
+      selecedCat: "Independence",
+      selectedLanguage: "",
+      category: [
+        {
+          itemName: "All",
+        },
+        {
+          itemName: "Independence",
+        },
+      ],
     };
     // this.setDate = this.setDate.bind(this);
     // this.setShowPicker = this.setShowPicker.bind(this);
@@ -225,6 +238,11 @@ export default class Details extends Component {
     }
   };
 
+  //Picker
+  async onValueChangeCat(value) {
+    this.setState({ selecedCat: value });
+  }
+
   render() {
     //Switch
     const { switch1Enabled, switch2Enabled, switch3Enabled } = this.state;
@@ -359,7 +377,7 @@ export default class Details extends Component {
                   <Text
                     style={[
                       styles.time,
-                      { Color: switch3Enabled ? "#333" : "#8A8A8A" },
+                      { color: switch3Enabled ? "#333" : "#8A8A8A" },
                     ]}
                   >
                     09:35
@@ -367,7 +385,7 @@ export default class Details extends Component {
                   <Text
                     style={[
                       styles.time,
-                      { Color: switch3Enabled ? "#333" : "#8A8A8A" },
+                      { color: switch3Enabled ? "#333" : "#8A8A8A" },
                     ]}
                   >
                     09:40
@@ -375,7 +393,7 @@ export default class Details extends Component {
                   <Text
                     style={[
                       styles.time,
-                      { Color: switch3Enabled ? "#333" : "#8A8A8A" },
+                      { color: switch3Enabled ? "#333" : "#8A8A8A" },
                     ]}
                   >
                     09:45
@@ -383,7 +401,7 @@ export default class Details extends Component {
                   <Text
                     style={[
                       styles.time,
-                      { Color: switch3Enabled ? "#333" : "#8A8A8A" },
+                      { color: switch3Enabled ? "#333" : "#8A8A8A" },
                     ]}
                   >
                     09:50
@@ -391,7 +409,7 @@ export default class Details extends Component {
                   <Text
                     style={[
                       styles.time,
-                      { Color: switch3Enabled ? "#333" : "#8A8A8A" },
+                      { color: switch3Enabled ? "#333" : "#8A8A8A" },
                     ]}
                   >
                     09:55
@@ -431,19 +449,6 @@ export default class Details extends Component {
                   </View>
                 </View>
               </Modal>
-              {/* Modal Setting Time */}
-              <Modal
-                animationType="slide"
-                transparent={true}
-                visible={settingTimeModal}
-                onRequestClose={() => {
-                  this.setSettingTimeModalVisible(!settingTimeModal);
-                }}
-              >
-                <View style={styles.modalContainer}>
-                  <View style={styles.modalContent}></View>
-                </View>
-              </Modal>
               <TouchableOpacity
                 style={styles.btnPlus}
                 onPress={this.toggleDatePicker}
@@ -481,13 +486,12 @@ export default class Details extends Component {
             >
               Farm 1
             </Text>
-            <View style={{width: '90%', marginBottom: 12}}>
-              <Text style={{color: 'white', textAlign: 'center'}}>
-              It is a long established fact that a reader will be distracted by
-              the readable
-            </Text>
+            <View style={{ width: "90%", marginBottom: 12 }}>
+              <Text style={{ color: "white", textAlign: "center" }}>
+                It is a long established fact that a reader will be distracted
+                by the readable
+              </Text>
             </View>
-            
           </SafeAreaView>
         </LinearGradient>
         <ScrollView style={styles.body} showsVerticalScrollIndicator={false}>
@@ -516,129 +520,166 @@ export default class Details extends Component {
           </View>
           <View style={styles.midle}>
             <View style={{ width: "95%" }}>
-              <Text style={styles.titleNote}>All control</Text>
-              <View style={styles.optionArea}>
-                <View style={{}}>
-                  <View style={styles.function}>
-                    <Text>Custom</Text>
-                    <Switch
-                      trackColor={{ false: "#767577", true: "#81b0ff" }}
-                      thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-                      ios_backgroundColor="#3e3e3e"
-                      onValueChange={this.handleSwitch1Change}
-                      value={switch1Enabled}
-                      style={{}}
-                    />
-                    <Text>Auto</Text>
-                    <Switch
-                      trackColor={{ false: "#767577", true: "#81b0ff" }}
-                      thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-                      ios_backgroundColor="#3e3e3e"
-                      onValueChange={this.handleSwitch2Change}
-                      value={switch2Enabled}
-                      style={{}}
-                    />
-                    <Slider
-                      style={{ width: 110, height: 40 }}
-                      minimumValue={50}
-                      maximumValue={95}
-                      value={sliderValue}
-                      onValueChange={this.handleSliderChange}
-                      onSlidingComplete={this.handleSliderComplete}
-                      minimumTrackTintColor={"#81BB4D"}
-                    />
-                    <Text>85%</Text>
-                  </View>
-                  <View style={styles.function}>
-                    <Text>Timer</Text>
-                    <Switch
-                      trackColor={{ false: "#767577", true: "#81b0ff" }}
-                      thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-                      ios_backgroundColor="#3e3e3e"
-                      onValueChange={this.handleSwitch3Change}
-                      value={switch3Enabled}
-                      style={{ marginLeft: 11 }}
-                    />
-                    <ScrollView
-                      horizontal={true}
-                      showsHorizontalScrollIndicator={false}
-                      style={[
-                        styles.timer,
-                        {
-                          backgroundColor: switch3Enabled ? "white" : "#D9D9D9",
-                        },
-                      ]}
-                    >
-                      <TouchableOpacity
-                        style={{ flexDirection: "row" }}
-                        onPress={() => this.setModalVisible(true)}
-                      >
-                        {/* Data time */}
-                        <Text
-                          style={[
-                            styles.time,
-                            { Color: switch3Enabled ? "#333" : "#8A8A8A" },
-                          ]}
-                        >
-                          09:35
-                        </Text>
-                        <Text
-                          style={[
-                            styles.time,
-                            { Color: switch3Enabled ? "#333" : "#8A8A8A" },
-                          ]}
-                        >
-                          09:40
-                        </Text>
-                        <Text
-                          style={[
-                            styles.time,
-                            { Color: switch3Enabled ? "#333" : "#8A8A8A" },
-                          ]}
-                        >
-                          09:45
-                        </Text>
-                        <Text
-                          style={[
-                            styles.time,
-                            { Color: switch3Enabled ? "#333" : "#8A8A8A" },
-                          ]}
-                        >
-                          09:50
-                        </Text>
-                        <Text
-                          style={[
-                            styles.time,
-                            { Color: switch3Enabled ? "#333" : "#8A8A8A" },
-                          ]}
-                        >
-                          09:55
-                        </Text>
-                      </TouchableOpacity>
-                    </ScrollView>
-                    {/* DateTimePicker */}
-                    {showPicker && (
-                      <DateTimePicker
-                        mode="time"
-                        display="spinner"
-                        value={dateTime}
-                        onChange={this.onChange}
+              <View style={styles.dropdownOptionArea}>
+                <Text style={styles.dropdownOptionText}>Control</Text>
+                <View>
+                  <Picker
+                    style={{ width: 180 }}
+                    mode="dropdown"
+                    selectedValue={this.state.selecedCat}
+                    onValueChange={this.onValueChangeCat.bind(this)}
+                  >
+                    {this.state.category.map((item, index) => (
+                      <Picker.Item
+                        color="#333"
+                        label={item.itemName}
+                        value={item.itemName}
+                        index={index}
                       />
-                    )}
-                    <TouchableOpacity
-                      style={styles.btnPlus}
-                      onPress={this.toggleDatePicker}
-                    >
-                      <Image
-                        source={require("../assets/img/plus.png")}
-                        style={styles.plusIcon}
-                      />
-                    </TouchableOpacity>
-                  </View>
+                    ))}
+                  </Picker>
                 </View>
               </View>
-              <Text style={styles.titleNote}>Custom control</Text>
-              {deviceList}
+              {this.state.selecedCat === "All" && (
+                <View>
+                  <Text style={styles.titleNote}>All control</Text>
+                  <View style={styles.optionArea}>
+                    <View style={styles.function}>
+                      <Text>Custom</Text>
+                      <Switch
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={this.handleSwitch1Change}
+                        value={switch1Enabled}
+                        style={{}}
+                      />
+                      <Text>Auto</Text>
+                      <Switch
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={this.handleSwitch2Change}
+                        value={switch2Enabled}
+                        style={{}}
+                      />
+                      <Slider
+                        style={{ width: 110, height: 40 }}
+                        minimumValue={50}
+                        maximumValue={95}
+                        value={sliderValue}
+                        onValueChange={this.handleSliderChange}
+                        onSlidingComplete={this.handleSliderComplete}
+                        minimumTrackTintColor={"#81BB4D"}
+                      />
+                      <Text>85%</Text>
+                    </View>
+                    <View style={styles.function}>
+                      <Text>Timer</Text>
+                      <Switch
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                        ios_backgroundColor="#3e3e3e"
+                        onValueChange={this.handleSwitch3Change}
+                        value={switch3Enabled}
+                        style={{ marginLeft: 11 }}
+                      />
+                      <ScrollView
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        style={[
+                          styles.timer,
+                          {
+                            backgroundColor: switch3Enabled
+                              ? "white"
+                              : "#D9D9D9",
+                          },
+                        ]}
+                      >
+                        <TouchableOpacity
+                          style={{ flexDirection: "row" }}
+                          onPress={() => this.setModalVisible(true)}
+                        >
+                          {/* Data time */}
+                          <Text
+                            style={[
+                              styles.time,
+                              { color: switch3Enabled ? "#333" : "#8A8A8A" },
+                            ]}
+                          >
+                            09:35
+                          </Text>
+                          <Text
+                            style={[
+                              styles.time,
+                              { color: switch3Enabled ? "#333" : "#8A8A8A" },
+                            ]}
+                          >
+                            09:40
+                          </Text>
+                          <Text
+                            style={[
+                              styles.time,
+                              { color: switch3Enabled ? "#333" : "#8A8A8A" },
+                            ]}
+                          >
+                            09:45
+                          </Text>
+                          <Text
+                            style={[
+                              styles.time,
+                              { color: switch3Enabled ? "#333" : "#8A8A8A" },
+                            ]}
+                          >
+                            09:50
+                          </Text>
+                          <Text
+                            style={[
+                              styles.time,
+                              { color: switch3Enabled ? "#333" : "#8A8A8A" },
+                            ]}
+                          >
+                            09:55
+                          </Text>
+                        </TouchableOpacity>
+                      </ScrollView>
+                      {/* DateTimePicker */}
+                      {showPicker && (
+                        <DateTimePicker
+                          mode="time"
+                          display="spinner"
+                          value={dateTime}
+                          onChange={this.onChange}
+                        />
+                      )}
+                      <TouchableOpacity
+                        style={styles.btnPlus}
+                        onPress={this.toggleDatePicker}
+                      >
+                        <Image
+                          source={require("../assets/img/plus.png")}
+                          style={styles.plusIcon}
+                        />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </View>
+              )}
+              {this.state.selecedCat === "Independence" && (
+                <View>
+                  <Text style={styles.titleNote}>Custom control</Text>
+                  {deviceList}
+                  {/* DateTimePicker */}
+                  {showPicker && (
+                    <DateTimePicker
+                      mode="time"
+                      display="spinner"
+                      value={dateTime}
+                      onChange={this.onChange}
+                    />
+                  )}
+                </View>
+              )}
             </View>
           </View>
         </ScrollView>
@@ -772,6 +813,26 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 5,
   },
+
+  //Drodown Option
+  dropdownOptionArea: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    marginTop: 5,
+    marginBottom: 5,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  dropdownOptionText: {
+    marginLeft: 10,
+    fontSize: 16,
+  },
   optionArea: {
     backgroundColor: "white",
     marginTop: 5,
@@ -832,8 +893,6 @@ const styles = StyleSheet.create({
   },
   modalContainer: {
     flex: 1,
-    // width: "100%",
-    // height: '70%',
     justifyContent: "flex-end",
   },
   modalContent: {
--- FAC_FarmAutomaticController/Views/Home.js
+++ FAC_FarmAutomaticController/Views/Home.js
@@ -6,39 +6,32 @@ import {
   View,
   TouchableOpacity,
   SafeAreaView,
-  Image,
-  FlatList,
   ScrollView,
   StatusBar,
 } from "react-native";
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
-} from "react-native-chart-kit";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import { Icon } from "@rneui/base";
+import { LinearGradient } from "expo-linear-gradient";
 import MyContext from "../DataContext.js";
 import { index } from "d3-array";
 // import axios from "axios";
 const data = [];
-//
+
+
 export default class Home extends Component {
   LoginPage = () => {
     console.log("Login Page");
     this.props.navigation.navigate("Login");
   };
+
   SignUpPage = () => {
     console.log("SignUp Page");
     this.props.navigation.navigate("SignUp");
   };
+
   DetailPage = (index) => {
     // console.log(index)
     this.GetEquidmentValues(index);
   };
+
   static contextType = MyContext;
   GetEquidmentValues = async (index) => {
     const { addDataAtIndex } = this.context;
@@ -102,43 +95,12 @@ export default class Home extends Component {
       <View style={styles.safeContainer}>
         <StatusBar backgroundColor="#ebf2f2" barStyle={"dark-content"} />
         <View style={styles.container}>
-          <View style={styles.header}>
-            <SafeAreaView>
-              <Text style={styles.monthText}>March</Text>
-              <View style={styles.calendarArea}>
-                <View style={styles.calendar}>
-                  <View style={{ alignItems: "center" }}>
-                    <Text style={{ color: "white" }}>Mon</Text>
-                    <Text style={styles.dayNumberText}>8</Text>
-                  </View>
-                  <View style={{ alignItems: "center" }}>
-                    <Text style={{ color: "white" }}>Tue</Text>
-                    <Text style={styles.dayNumberText}>9</Text>
-                  </View>
-                  <View style={{ alignItems: "center" }}>
-                    <Text style={{ color: "white" }}>Wed</Text>
-                    <Text style={styles.dayNumberText}>10</Text>
-                  </View>
-                  <View style={[styles.active, { alignItems: "center" }]}>
-                    <Text style={{ color: "white" }}>Thu</Text>
-                    <Text style={styles.dayNumberText}>11</Text>
-                  </View>
-                  <View style={{ alignItems: "center" }}>
-                    <Text style={{ color: "white" }}>Fri</Text>
-                    <Text style={styles.dayNumberText}>12</Text>
-                  </View>
-                  <View style={{ alignItems: "center" }}>
-                    <Text style={{ color: "white" }}>Sat</Text>
-                    <Text style={styles.dayNumberText}>13</Text>
-                  </View>
-                  <View style={{ alignItems: "center" }}>
-                    <Text style={{ color: "white" }}>Sun</Text>
-                    <Text style={styles.dayNumberText}>14</Text>
-                  </View>
-                </View>
-              </View>
+          <LinearGradient colors={["#bfd200", "#aacc00", "#80b918"]} style={styles.header}>
+            <SafeAreaView style={styles.header}>
+              <Text style={styles.headerText}>Hello! Have a nice Day</Text>
+              <Text style={styles.headerText}>NQD2308</Text>
             </SafeAreaView>
-          </View>
+          </LinearGradient>
           <View style={styles.body}>
             {/* <MyContext.Consumer>
                     {contextData => {
@@ -151,7 +113,7 @@ export default class Home extends Component {
               showsVerticalScrollIndicator={false}
             >
               <View style={{ marginTop: 8 }}>
-                <View>{farmHouseList}</View>
+                {farmHouseList}
               </View>
             </ScrollView>
           </View>
@@ -175,42 +137,21 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 150,
     marginBottom: 6,
-    backgroundColor: "#80b918",
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: "#80b918",
     borderBottomRightRadius: 30,
     borderBottomLeftRadius: 30,
+  },
+  headerText: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: 'white'
   },
   body: {
     width: "90%",
     marginTop: 6,
     justifyContent: "center",
-  },
-  calendarArea: {
-    alignItems: "center",
-  },
-  calendar: {
-    width: "90%",
-    marginTop: 12,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  active: {
-    padding: 3,
-    borderWidth: 1.2,
-    borderColor: "white",
-    borderRadius: 8,
-  },
-  monthText: {
-    marginLeft: 40,
-    marginBottom: 12,
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 18,
-  },
-  dayNumberText: {
-    fontWeight: "bold",
-    marginTop: 5,
-    color: "white",
   },
   famrItem: {
     padding: 8,
