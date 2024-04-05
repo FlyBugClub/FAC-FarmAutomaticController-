830d3e171edfa4ae2d11184aac3672b3b6a2d5a2
duc <taduc1130@gmail.com>
2024-04-05 21:31:48 +0700

    this code can read https

--- MushroomAutomation/MushroomAutomation.ino
+++ MushroomAutomation/MushroomAutomation.ino
@@ -1,158 +1,49 @@
-#include "secret_pass.h"
-#include <Wire.h>
-#include "Adafruit_SHT31.h"
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
 #include <ESP8266WiFi.h>
-#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
 
-const char* serverUrl = "https://ngunemay123.bsite.net/api/login/admin@gmail.com/123456";
-const int pumpPin = D6;
-bool autoControl = true;
-float desiredTemperature = 25.0;
-float desiredHumidity = 60.0;
-unsigned long lastSprayTime = 0;
-bool wifiConnected = false;
-bool sensorConnected = false;
-
-Adafruit_SHT31 sht31 = Adafruit_SHT31();
+const char* ssid = "Thu Tamm";
+const char* password = "thutam1975";
+const char* api_url = "https://ngunemay123.bsite.net"; // Thêm giao thức HTTPS vào URL
 
 void setup() {
-  pinMode(pumpPin, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("SHT31 test");
+  Serial.begin(115200);
+  delay(100);
 
-  connectToWiFi();
-}
-
-void reconnectSensor() {
-  Serial.println("Attempting to reconnect sensor...");
-  
-  if (!sht31.begin(0x44)) {
-    Serial.println("Couldn't find SHT31. Retrying...");
-    delay(1000);
-    return;
-  }
-
-  Serial.println("Sensor reconnected successfully.");
-  sensorConnected = true;
-}
-
-void loop() {
-  if (!wifiConnected) {
-    connectToWiFi();
-  }
-
-  // if (!sensorConnected) {
-  //   reconnectSensor();
-  // } else {
-  //   float temperature = sht31.readTemperature();
-  //   float humidity = sht31.readHumidity();
-  //   manageAutoControl();
-  // }
-
-   String jsonString;
-    if (getDataFromAPI(serverUrl, jsonString)) {
-      Serial.println("Received JSON data:");
-      Serial.println(jsonString);
-    } else {
-      Serial.println("Failed to get data from API");
-    }
-  
-  
-}
-
-void connectToWiFi() {
-  WiFi.begin(ssid, pass);
-  unsigned long startTime = millis();
-  
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
-    if (millis() - startTime > 30000) {
-      Serial.println("WiFi connection timeout!");
-      break;
-    }
   }
+  Serial.println("Connected to WiFi");
 
-  if (WiFi.status() == WL_CONNECTED) {
-    wifiConnected = true;
-    Serial.println("WiFi connected");
-    autoControl = true;
-
-    // Gọi hàm sendDataToAPI() ở đây nếu bạn muốn gửi dữ liệu ngay sau khi kết nối WiFi thành công
-  } else {
-    wifiConnected = false;
-    Serial.println("WiFi connection failed");
-    autoControl = false;
-    digitalWrite(pumpPin, LOW);
-  }
-}
-
-void manageAutoControl() {
-  if (autoControl) {
-    autoControlMode(desiredTemperature, desiredHumidity);
-  }
-}
-
-void autoControlMode(float& temperature, float& humidity) {
-  float currentHumidity = sht31.readHumidity();
-  unsigned long currentMillis = millis();
-
-  if (currentHumidity < humidity && currentMillis - lastSprayTime >= 10000) {
-    digitalWrite(pumpPin, HIGH);
-    lastSprayTime = currentMillis;
-    delay(2000);
-    digitalWrite(pumpPin, LOW);
-  } else {
-    digitalWrite(pumpPin, LOW);
-  }
-}
-
-void sendDataToAPI(float temperature, float humidity) {
+  // Make HTTP request
+  WiFiClientSecure client;
   HTTPClient http;
-  WiFiClient client; // Tạo một đối tượng WiFiClient
-  
-  StaticJsonDocument<200> jsonBuffer;
-  JsonObject jsonData = jsonBuffer.to<JsonObject>();
+  http.begin(client, api_url); // Bắt đầu yêu cầu HTTP với URL HTTPS
 
-  jsonData["temperature"] = temperature;
-  jsonData["humidity"] = humidity;
+  // Vô hiệu hóa kiểm tra chứng chỉ SSL
+  client.setInsecure();
 
-  String jsonString;
-  serializeJson(jsonData, jsonString);
-
-  http.begin(client, serverUrl);
+  http.setReuse(true); // Sử dụng lại kết nối TCP
   http.addHeader("Content-Type", "application/json");
 
-  int httpResponseCode = http.POST(jsonString);
+  int httpCode = http.GET();
+  Serial.println("httpCode");
+  Serial.println(httpCode);
 
-  if (httpResponseCode == 200) {
-    Serial.println("Data sent successfully");
+  if (httpCode > 0) {
+    String payload = http.getString();
+    Serial.println("Received JSON:");
+    Serial.println(payload);
   } else {
-    Serial.print("Error sending data. HTTP response code: ");
-    Serial.println(httpResponseCode);
+    Serial.print("HTTP request failed with error code: ");
+    Serial.println(httpCode);
   }
 
   http.end();
 }
 
-bool getDataFromAPI(const char* url, String& jsonString) {
-  HTTPClient http;
-  WiFiClient client; // Tạo một đối tượng WiFiClient
-
-  // Send HTTP GET request
-  http.begin(client, serverUrl, 443, "/path", true);
-  int httpResponseCode = http.GET();
-
-  if (httpResponseCode == HTTP_CODE_OK) {
-    jsonString = http.getString();
-    return true;
-  } else {
-    Serial.print("HTTP request failed with error code: ");
-    Serial.println(httpResponseCode);
-    return false;
-  }
-
-  http.end();
+void loop() {
+  // Your code here
 }
