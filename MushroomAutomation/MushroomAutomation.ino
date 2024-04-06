df8e1805a89d585568da1935ab8f8be7ebfdbd85
duc <taduc1130@gmail.com>
2024-04-06 19:04:12 +0700

    clearer code

--- MushroomAutomation/MushroomAutomation.ino
+++ MushroomAutomation/MushroomAutomation.ino
@@ -1,48 +1,48 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
-#include "secret_pass.h"
+// Includes
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <ArduinoJson.h>
+  #include "secret_pass.h"
 
-#include <Wire.h>
-#include "Adafruit_SHT31.h"
+  #include <Wire.h>
+  #include "Adafruit_SHT31.h"
 
-#include <NTPClient.h>
-#include <WiFiUdp.h>
+  #include <NTPClient.h>
+  #include <WiFiUdp.h>
 
 // Constants
-const char* ssid = "your_ssid";
-const char* pass = "your_password";
-const char* ntpServer = "pool.ntp.org";
-const int ntpPort = 123;
-const char* server_address = "ngunemay123.bsite.net";
-const int server_port = 443;
-const int pumpPin = D6;
-const long timeZoneOffset = 7 * 3600;
-const unsigned long sprayInterval = 10000; // 10 seconds
+
+  const char* ntpServer = "pool.ntp.org";
+  const int ntpPort = 123;
+  const char* server_address = "ngunemay123.bsite.net";
+  const int server_port = 443;
+  const int pumpPin = D6;
+  const long timeZoneOffset = 7 * 3600;
+  const unsigned long sprayInterval = 10000; // 10 seconds
 
 // Variables
-bool enableHeater = false;
-uint8_t loopCnt = 0;
-Adafruit_SHT31 sht31;
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, ntpServer, ntpPort);
-bool autoControl = true;
-float desiredTemperature = 25.0;
-float desiredHumidity = 60.0;
-unsigned long lastSprayTime = 0;
-bool wifiConnected = false;
-int pumpActivationCount = 0;
-int previousDay = -1;
+  bool enableHeater = false;
+  uint8_t loopCnt = 0;
+  Adafruit_SHT31 sht31;
+  WiFiUDP ntpUDP;
+  NTPClient timeClient(ntpUDP, ntpServer, ntpPort);
+  bool autoControl = true;
+  float desiredTemperature = 25.0;
+  float desiredHumidity = 60.0;
+  unsigned long lastSprayTime = 0;
+  bool wifiConnected = false;
+  int pumpActivationCount = 0;
+  int previousDay = -1;
 
 // Function prototypes
-void setup();
-void loop();
-void connectToWiFi();
-void getCurrentDateTime(String& formattedDateTime);
-void postHumidityToAPI(const char* url, String formattedDateTime);
-void postCountPumpToAPI(const char* url, String formattedDateTime);
-void autoControlMode(float& temperature, float& humidity);
-bool isNewDay(String formattedTime);
+  void setup();
+  void loop();
+  void connectToWiFi();
+  void getCurrentDateTime(String& formattedDateTime);
+  void postHumidityToAPI(const char* url, String formattedDateTime);
+  void postCountPumpToAPI(const char* url, String formattedDateTime);
+  void autoControlMode(float& temperature, float& humidity);
+  bool isNewDay(String formattedTime);
 
 void setup() {
   pinMode(pumpPin, OUTPUT);
@@ -64,14 +64,12 @@ void loop() {
   if (!wifiConnected) {
     connectToWiFi();
   }
-
+  
   String formattedDateTime;
   getCurrentDateTime(formattedDateTime);
-  Serial.println("Current Date and Time: " + formattedDateTime);
-
-  postHumidityToAPI("/api/sensorvalues", formattedDateTime);
-  postCountPumpToAPI("/api/equidmentvalues", formattedDateTime);
-
+  // countPumpActivations(formattedDateTime);
+  // postHumidityToAPI("/api/sensorvalues", formattedDateTime);
+  getAndParseAPI("/api/login/admin@gmail.com/123456");
   delay(1000);
 }
 
@@ -214,4 +212,81 @@ bool isNewDay(String formattedTime) {
     return true;
   }
   return false;
+}
+
+
+void getAndParseAPI(const char* url) {
+  WiFiClientSecure client;
+  HTTPClient http;
+
+  String api_url = "https://" + String(server_address) + url;
+
+  http.begin(client, api_url);
+  client.setInsecure();
+
+  int httpCode = http.GET();
+  
+  static char payload[256];
+
+  if (httpCode > 0) {
+    http.getString().toCharArray(payload, sizeof(payload));
+    http.end();
+    
+    StaticJsonDocument<256> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+
+    if (!error) {
+      JsonObject user = doc[0]["user"];
+      const char* userId = user["id"];
+      const char* userEmail = user["gmail"];
+      const char* userName = user["name"];
+      const char* userPhone = user["phone"];
+      int membership = user["membership"].as<int>();  
+
+      JsonObject sensor = doc[0]["0"];
+      const char* espId = sensor["id_esp"];
+      const char* espName = sensor["name"];
+      int bcValue = sensor["bc"].as<int>();    
+      int dhtValue = sensor["dht"].as<int>();  
+      int phValue = sensor["ph"].as<int>();    
+    }
+  } else {
+    // Trả về NULL nếu yêu cầu không thành công
+    Serial.print("GET request failed with error code: ");
+    Serial.println(httpCode);
+    http.end();
+  }
+}
+
+
+
+void reconnectSensor() {
+  Serial.println("Attempting to reconnect sensor...");
+
+  // Thử khởi tạo lại cảm biến
+  if (!sht31.begin(0x44)) {  // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31. Retrying...");
+    delay(1000);
+    return;  // Thử lại sau một khoảng thời gian
+  }
+}
+
+
+void manageAutoControl() {
+  if (autoControl) {
+    autoControlMode(desiredTemperature, desiredHumidity);
+  }
+}
+
+
+void countPumpActivations(String formattedTime) {
+  // Kiểm tra xem đã qua ngày mới chưa
+  if (isNewDay(formattedTime)) {
+    pumpActivationCount = 0;
+  }
+  if (digitalRead(pumpPin) == HIGH) {
+    pumpActivationCount++;
+    postCountPumpToAPI("/api/equidmentvalues", formattedTime);
+
+  }
 }
