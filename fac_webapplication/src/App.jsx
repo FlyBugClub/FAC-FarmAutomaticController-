f5ee49bae14d0cdcc6ad0d929a2bd87c5612ea8b
duc <taduc1130@gmail.com>
2024-05-28 09:01:02 +0700

    blank login

--- fac_webapplication/src/App.jsx
+++ fac_webapplication/src/App.jsx
@@ -1,10 +1,9 @@
-
+import Login from "./components/Login/Login";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-        </p>
+      <Login/>
         
       </header>
     </div>
---
+++ fac_webapplication/src/components/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Login.scss';
+
+const Login = () => {
+  return (
+    <div className="Login" >    
+      <div>123</div>
+      <div>123123</div>
+    </div>
+  );
+}
+
+export default Login;
