e04642ff1c639662ee8c715ceb27030cf906ea8f
Đinh Quốc Cường <tr6r20@gmail.com>
2024-05-28 14:04:40 +0700

    update menu.jsx

--- fac_webapplication/src/App.jsx
+++ fac_webapplication/src/App.jsx
@@ -1,9 +1,9 @@
-
+import { Dashboard } from "./components/Dashboard/dashboard";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Login/>
+      <Dashboard/>
         
       </header>
     </div>
--- fac_webapplication/src/components/Menu/menu.jsx
+++ fac_webapplication/src/components/Menu/menu.jsx
@@ -1,10 +1,34 @@
 import React, { useEffect, useState, PureComponent } from "react";
+import { FiGrid,FiUserMinus ,FiSettings} from "react-icons/fi";
+import { IoNotifications } from "react-icons/io5";
+
 import './menu.scss'
 const Menu = () => {
 
     return (
         <div className="Fac_Menu">
-           
+            <div className="Fac_Menu_User">
+                <img className="Fac_Menu_User_Avartar" src="./images/user.png" alt=""/>
+                <div className="Fac_Menu_User_Name" >User name</div>
+            </div>
+            <div className="Fac_Menu_Options">
+                <div className="Fac_Menu_Options_Elements">
+                <FiGrid size={22}/>
+                    <div style={{marginLeft:"10px"}}>Dashboard</div>
+                </div>
+                <div className="Fac_Menu_Options_Elements">
+                <IoNotifications size={22}/>
+                    <div style={{marginLeft:"10px"}}>Notification</div>
+                </div>
+                <div className="Fac_Menu_Options_Elements">
+                <FiSettings size={22}/>
+                    <div style={{marginLeft:"10px"}}>Setting</div>
+                </div>
+                <div className="Fac_Menu_Options_Elements">
+                <FiUserMinus size={22}/>
+                    <div style={{marginLeft:"10px"}}>Logout</div>
+                </div>
+            </div>
 
         </div>
     )
