788ae3f0c38428685cbb01863920d3eceaefb600
duc <taduc1130@gmail.com>
2024-05-28 09:23:05 +0700

    BrowserView

--- fac_webapplication/src/components/Login/Login.jsx
+++ fac_webapplication/src/components/Login/Login.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import {
+  BrowserView,
+  MobileView,
+ 
+} from "react-device-detect";
+import React from "react";
 
-import './Login.scss';
+import "./Login.scss";
 
 const Login = () => {
   return (
-    <div className="Login" >    
-      <div>123</div>
-      <div>123123</div>
+    <div>
+      <BrowserView>
+        <div className="Login">
+          <img src="images/CanhDong.jpg" alt="" style={{ width: "100%" }} />
+          <div className="Login"></div>
+        </div>
+      </BrowserView>
+      <MobileView>
+        <h1>This is rendered only on mobile</h1>
+      </MobileView>
     </div>
   );
-}
+};
 
 export default Login;
