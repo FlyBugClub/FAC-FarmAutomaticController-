781e25eff474996de81785d6ceb254b9e93915ca
duc <taduc1130@gmail.com>
2024-05-31 11:52:48 +0700

    react-toastify

--- fac_webapplication/src/App.jsx
+++ fac_webapplication/src/App.jsx
@@ -5,8 +5,11 @@ import Signup from "./components/Auth/Signup";
 import ForgotPassw from "./components/Auth/ForgotPassw";
 import { Dashboard } from "./components/Dashboard/dashboard";
 import NewPassw from "./components/Auth/NewPassw";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
+    
     <Router>
       <div className="App">
         <header className="App-header">
@@ -19,7 +22,10 @@ function App() {
           </Routes>
         </header>
       </div>
+      <ToastContainer />
     </Router>
+      
+
   );
 }
 
