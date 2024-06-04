e7cb0ae64fc87ea5cd8fb818a1700c3114e5ca77
duc <taduc1130@gmail.com>
2024-06-04 15:38:00 +0700

    validate forgotpass

--- fac_webapplication/src/components/Auth/ForgotPassw.jsx
+++ fac_webapplication/src/components/Auth/ForgotPassw.jsx
@@ -1,12 +1,52 @@
 import { BrowserView, MobileView } from "react-device-detect";
 import { useNavigate } from 'react-router-dom';
-import React from "react";
+import React, { useState } from "react";
 import { FiMail} from "react-icons/fi";
 import "./Auth.scss";
-
+//
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+//
+import { callAPi } from "../../services/UserService";
+//
+import { checkEmail } from "../../validation";
 const ForgotPassw = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const validateEmail = (email) => {
+    if (email.trim() === "") {
+      toast.error("Vui lòng nhập email")
+      return false;
+    }
+    if (!checkEmail(email)) {
+      toast.error("Vui lòng nhập email đúng định dạng")
+      return false
+      
+    }
+    return true;
+  }
+  const handleChangEmail = (e) => {
+    const newEmail = e.target.value
+    setEmail(newEmail);
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const isEmailValid = validateEmail(email);
+    
 
+    if (isEmailValid ) {
+      const checkApi = async () => {
+        let res = await callAPi('post',`http://61.28.230.132:3004/auth/Login`, {
+            username: 'ndtt',
+            password: 'abc123'
+        })
+
+        console.log(res)
+    }
+    checkApi()
+    }
+    
+  };
   return (
     <div className="Auth">
       <BrowserView className="Auth_BrowserView">
@@ -20,17 +60,27 @@ const ForgotPassw = () => {
               <div className="div2">Giải pháp hoàn hảo cho nhà nông</div>
             </div>
           </div>
-        <div className="Auth_BrowserView_Region-Forgot">
+          
+        <form 
+        className="Auth_BrowserView_Region-Forgot"
+        onSubmit={handleSubmit}
+        >
+
           <div className="Auth_BrowserView_Region-Forgot_Input ">
             <div>
               <FiMail  color="white" size={24} />
             </div>
-            <input type="text" placeholder="Email"></input>
+            <input
+             type="text" 
+             placeholder="Email"
+             value={email}
+             onChange={handleChangEmail}
+             ></input>
           </div>
           <div className="Auth_BrowserView_Region-Forgot_Button">
-            <button type="submit"onClick={() => navigate('/newpassw')} >Gửi</button>
+            <button type="submit">Gửi</button>
           </div>
-        </div>
+        </form>
         </div>
       </BrowserView>
 
