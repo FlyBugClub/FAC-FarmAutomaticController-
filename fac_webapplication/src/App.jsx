2b3dc1388eea89ca9e07e9ffd739bf6b2591a393
Đinh Quốc Cường <tr6r20@gmail.com>
2024-06-07 14:01:38 +0700

    update route 2

--- fac_webapplication/src/App.jsx
+++ fac_webapplication/src/App.jsx
@@ -13,9 +13,14 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [weatherState, setWeatherState] = useState(true);
+  const [loginState,setLoginState] = useState(false);
 
-
-
+  useEffect(() => {
+    console.log(loginState)
+  }, [loginState])
+  const handleLogin = () => {
+    setLoginState(!loginState);
+  }
 
   const handleWeather = () => {
     setWeatherState(!weatherState);
@@ -25,22 +30,35 @@ function App() {
     <Router>
       <div className="App" >
         <header className="App-header">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/forgotpassword" element={<ForgotPassw />} />
-            <Route path="/newpassw" element={<NewPassw />} />
-            <Route
-              path="/home/dashboard"
-              element={
+          {
+            loginState ?
                 <div style={{ position: "fixed", width: "100%", height: "100%" }}>
                   <img src="/images/b3.jpg" alt="" style={{ position: "fixed", zIndex: "-1", width: "100%", height: "100%", filter: "brightness(0.9)" }} />
                   <Menu handleWeather={handleWeather} weatherState={weatherState} />
                   <div style={{ display: "flex", width: "100%", height: `calc(100vh - 50px)`, padding: "15px", boxSizing: "border-box" }}>
                     <Weather weatherState={weatherState} />
-                    <Dashboard  weatherState={weatherState}/>
+                    <Routes>
+                      <Route path="/dashboard" element={<Dashboard  weatherState={weatherState}/>} />
+                      <Route path="/farm" element={<Farm  weatherState={weatherState}/>} />
+
+                      <Route path="/" element={<Navigate to="/dashboard" />} />
+                      <Route path="/login" element={<Navigate to="/dashboard" />} />
+                    </Routes>
+                    
                   </div>
                 </div>
+           
+            :
+            <Routes>
+
+            <Route path="/login" element={<Login handleLogin={handleLogin}/>} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgotpassword" element={<ForgotPassw />} />
+            <Route path="/newpassw" element={<NewPassw />} />
+            
+            {/* <Route
+              path="/home/dashboard"
+              element={
               }
             />
               <Route
@@ -51,17 +69,19 @@ function App() {
                   <Menu handleWeather={handleWeather} weatherState={weatherState} />
                   <div style={{ display: "flex", width: "100%", height: `calc(100vh - 50px)`, padding: "15px", boxSizing: "border-box" }}>
                     <Weather weatherState={weatherState} />
-                    <Farm  weatherState={weatherState}/>
+                    
                   </div>
                 </div>
               }
-            />
+            /> */}
 
             {/* <Route path="/home/*" element={<Navigate to="/home/dashboard" />} /> */}
-            <Route path="/home" element={<Navigate to="/home/dashboard" />} />
+            {/* <Route path="/home" element={<Navigate to="/home/dashboard" />} /> */}
 
-            <Route path="*" element={<Navigate to="/login" />} />
+            <Route path="/" element={<Navigate to="/login" />} />
           </Routes>
+          }
+          
         </header>
 
       </div>
