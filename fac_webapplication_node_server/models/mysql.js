234fd7b67426fe8c2dffd1db1b2435547f6771d7
duc <taduc1130@gmail.com>
2024-06-11 09:22:40 +0700

    UPDATE EDIT DELETE

--- fac_webapplication_node_server/models/mysql.js
+++ fac_webapplication_node_server/models/mysql.js
@@ -29,20 +29,63 @@ const connection = async () => {
   }
 };
 
-const SELECT = async (select,table,condition) => {
+const SELECT = async (select, table, condition) => {
   return new Promise(async (resolve, reject) => {
-
-    let condition_ = ""
-    if(condition){
-        condition_ = condition
-    }else{
-      condition_ = ""
+    let condition_ = "";
+    if (condition) {
+      condition_ = condition;
+    } else {
+      condition_ = "";
     }
 
-    const result = await sql.query("select "+select+" from "+table+" "+condition_+"")
+    const result = await sql.query(
+      "select " + select + " from " + table + " " + condition_ + ""
+    );
 
     resolve(result);
   });
+};
+const UPDATE = async (table_name, update_values, condition) => {
+  return new Promise(async (resolve, reject) => {
+    let condition_ = "";
+    if (condition) {
+      condition_ = condition;
+    } else {
+      condition_ = "";
+    }
+
+    const result = await sql.query(
+      `update ${table_name} set ${update_values} ${condition_}`
+    );
+
+    resolve(result);
+  });
+};
+
+const EDIT = async (table_name, update_values, condition) => {
+  let condition_ = "";
+  if (condition) {
+    condition_ = condition;
+  }
+
+  const result = await sql.query(
+    `update ${table_name} set ${update_values} ${condition_}`
+  );
+
+  return result;
+};
+
+const DELETE = async (table_name, condition) => {
+  let condition_ = "";
+  if (condition) {
+    condition_ = condition;
+  }
+
+  const result = await sql.query(
+    `delete from ${table_name} where ${condition_}`
+  );
+
+  return result;
 };
 
 module.exports = { connection, SELECT };
