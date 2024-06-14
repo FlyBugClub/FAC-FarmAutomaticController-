663768fc844921600598d36e51664a92187869fd
duc <taduc1130@gmail.com>
2024-06-14 09:51:00 +0700

    WrappedApp

--- fac_webapplication/src/App.jsx
+++ fac_webapplication/src/App.jsx
@@ -6,6 +6,7 @@ import {
   Redirect,
   Navigate,
   Routes,
+  useNavigate,
 } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
@@ -21,17 +22,12 @@ import Weather from "./components/Weather/weather";
 import Farm from "./components/Home/farm";
 import Addfarm from "./components/Home/newfarm";
 import User from "./components/Home/user";
+
 function App() {
-  // const [user, setUser] = useState(false);
-  // setUser(JSON.parse(localStorage.getItem("user")));
-  // useEffect(() => {
-  //   const checkApi = async () => {
-  //     let res = await callAPi("post", `http://61.28.230.132:3004/auth/Login`, {
-  //       username: "ndtt",
-  //       password: "abc123",
-  //     });
+  const navigate = useNavigate();
   const [weatherState, setWeatherState] = useState(true);
-  const [addDeviceState,setAddDeviceState] = useState("")
+  const [addDeviceState, setAddDeviceState] = useState("");
+  
   const handleWeather = () => {
     setWeatherState(!weatherState);
   };
@@ -40,95 +36,83 @@ function App() {
   };
 
   const Context = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!Context.isLoggedIn) {
+      navigate("/login");
+    }
+  }, [Context.isLoggedIn, navigate]);
+
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          {Context.isLoggedIn ? (
-            <div style={{ position: "fixed", width: "100%", height: "100%" }}>
-              <img
-                src="/images/b3.jpg"
-                alt=""
-                style={{
-                  position: "fixed",
-                  zIndex: "-1",
-                  width: "100%",
-                  height: "100%",
-                  filter: "brightness(0.9)",
-                }}
-              />
-              <Menu handleWeather={handleWeather} weatherState={weatherState} />
-              <div
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  height: `calc(100vh - 50px)`,
-                  padding: "15px",
-                  boxSizing: "border-box",
-                  position: "fixed",
-                  zIndex: "-1",
-                }}
-              >
-                <Weather weatherState={weatherState} />
-                <Routes>
-                  <Route
-                    path="/dashboard"
-                    element={<Dashboard weatherState={weatherState} handleAddDevice={handleAddDevice} />}
-                  />
-                  <Route
-                    path="/farm"
-                    element={<Farm weatherState={weatherState} handleAddDevice={handleAddDevice}/>}
-                  />
-                  <Route
-                    path="/addfarm"
-                    element={<Addfarm weatherState={weatherState} addDeviceState={addDeviceState} />}
-                  />
-                  <Route
-                    path="/usersetting"
-                    element={<User weatherState={weatherState} />}
-                  />
-                  <Route path="*" element={<Navigate to="/dashboard" />} />
-                </Routes>
-              </div>
-            </div>
-          ) : (
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/" element={<Navigate to="/login" />} />
-              <Route path="/signup" element={<Signup/>} />
-              <Route path="/forgotpassword" element={<ForgotPassw />} />
-              <Route path="/newpassw" element={<NewPassw />} />
-
-              {/* <Route
-              path="/home/dashboard"
-              element={
-              }
+    <div className="App">
+      <header className="App-header">
+        {Context.isLoggedIn ? (
+          <div style={{ position: "fixed", width: "100%", height: "100%" }}>
+            <img
+              src="/images/b3.jpg"
+              alt=""
+              style={{
+                position: "fixed",
+                zIndex: "-1",
+                width: "100%",
+                height: "100%",
+                filter: "brightness(0.9)",
+              }}
             />
-              <Route
-              path="/home/farm"
-              element={
-                <div style={{ position: "fixed", width: "100%", height: "100%" }}>
-                  <img src="/images/b3.jpg" alt="" style={{ position: "fixed", zIndex: "-1", width: "100%", height: "100%", filter: "brightness(0.9)" }} />
-                  <Menu handleWeather={handleWeather} weatherState={weatherState} />
-                  <div style={{ display: "flex", width: "100%", height: `calc(100vh - 50px)`, padding: "15px", boxSizing: "border-box" }}>
-                    <Weather weatherState={weatherState} />
-                    
-                  </div>
-                </div>
-              }
-            /> */}
-
-              {/* <Route path="/home/*" element={<Navigate to="/home/dashboard" />} /> */}
-              {/* <Route path="/home" element={<Navigate to="/home/dashboard" />} /> */}
-
-              <Route path="/" element={<Navigate to="/login" />} />
-            </Routes>
-          )}
-        </header>
-      </div>
+            <Menu handleWeather={handleWeather} weatherState={weatherState} />
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                height: `calc(100vh - 50px)`,
+                padding: "15px",
+                boxSizing: "border-box",
+                position: "fixed",
+                zIndex: "-1",
+              }}
+            >
+              <Weather weatherState={weatherState} />
+              <Routes>
+                <Route
+                  path="/dashboard"
+                  element={<Dashboard weatherState={weatherState} handleAddDevice={handleAddDevice} />}
+                />
+                <Route
+                  path="/farm"
+                  element={<Farm weatherState={weatherState} handleAddDevice={handleAddDevice} />}
+                />
+                <Route
+                  path="/addfarm"
+                  element={<Addfarm weatherState={weatherState} addDeviceState={addDeviceState} />}
+                />
+                <Route
+                  path="/usersetting"
+                  element={<User weatherState={weatherState} />}
+                />
+                <Route path="*" element={<Navigate to="/dashboard" />} />
+              </Routes>
+            </div>
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgotpassword" element={<ForgotPassw />} />
+            <Route path="/newpassw" element={<NewPassw />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        )}
+      </header>
       <ToastContainer />
-    </Router>
+    </div>
   );
 }
 
-export default App;
+export default function WrappedApp() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
