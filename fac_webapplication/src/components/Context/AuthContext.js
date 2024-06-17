d2406ed4200a45bcc4aaafa8763a1823e4eb53f7
duc <taduc1130@gmail.com>
2024-06-17 16:11:48 +0700

    context template

---
+++ fac_webapplication/src/components/Context/AuthContext.js
@@ -0,0 +1,27 @@
+import { createContext, useContext, useReducer } from "react";
+//import Logger from "../Logger";
+import ToolReducer,{INITIAL_STATE} from "./ToolReducer";
+
+export const ToolContext = createContext(INITIAL_STATE);
+
+const [state, toolDispatch] = useContext(ToolContext, INITIAL_STATE);
+
+export const ToolContextProvider =({children})=>{
+    const [state, toolDispatch] = useReducer(ToolReducer, INITIAL_STATE);
+
+    return (
+        <ToolContext.Provider value = {{
+                config:state.config,
+                visual:state.visual,
+                setting:state.setting,
+                lastid:state.lastid,
+                name:state.name,
+                control:state.control,
+                toolDispatch
+            }}
+        >
+            {children}
+        </ToolContext.Provider>
+    )
+
+}
