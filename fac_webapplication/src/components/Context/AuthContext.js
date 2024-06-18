ebb77cc96a90f4365862deae1c903e6a065957b4
duc <taduc1130@gmail.com>
2024-06-18 10:37:45 +0700

    del unuse

--- fac_webapplication/src/components/Context/AuthContext.js
+++ fac_webapplication/src/components/Context/AuthContext.js
@@ -3,10 +3,6 @@ import { createContext, useReducer } from "react";
 import AuthReducer, {INITIAL_STATE} from "../Context/AuthReducer";
 export const AuthContext = createContext(INITIAL_STATE);
 
-// const apiURL = "http://10.101.172.53:8080";// run on server
-// const apiURL = "http://103.130.211.141:8080"; //http://103.130.211.141:8080/ run on local
-const apiURL = "http://172.31.8.230:3001"; // run on dat
-
 export const AuthContextProvider =({children})=>{
     const [state, authDispatch] = useReducer(AuthReducer, INITIAL_STATE);
     return (
