e2ead54977615ab9031993bbb89aeeab5c9e1c20
NQD2308 <nqd230899@gmail.com>
2024-06-19 14:10:30 +0700

    re-design login page

--- fac_webapplication/src/components/Auth/Login.jsx
+++ fac_webapplication/src/components/Auth/Login.jsx
@@ -69,7 +69,7 @@ const Login = () => {
     // };
     // checkApi();
     console.log(login.status);
-    console.log(user.id_user_)
+    console.log(user.id_user_);
   };
   const sendToken = (check) => {
     if (check) {
@@ -101,7 +101,7 @@ const Login = () => {
             authDispatch({
               type: "SET_USER",
               payload: res.data[0],
-            })
+            });
             // loginContext.login(checkSavePassword);
             console.log(login);
           } else {
@@ -109,7 +109,7 @@ const Login = () => {
             authDispatch({
               type: "SET_USER",
               payload: res.data[0],
-            }) 
+            });
             authDispatch({
               type: "SET_LOGIN",
               payload: { status: true, isSave: checkSavePassword },
@@ -189,52 +189,90 @@ const Login = () => {
 
       <MobileView className="Auth_MobileView">
         <div style={{ width: "100%", height: "100%" }}>
-          <div className="Auth_MobileView_Logo">
-            <div className="Auth_MobileView_Logo_Image">
-              <img src="/icons/plantbackground1.jpg" alt="" />
+          <div className="Auth_MobileView_BGTop">
+            <div className="Auth_MobileView_BGTop_Bg">
+              {/* <img src="/images/Rectangle01.png" alt="" /> */}
+              <svg
+                class="wave"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1440 320"
+              >
+                <path
+                  fill="#fff"
+                  fill-opacity="1"
+                  d="M0,64L40,69.3C80,75,160,85,240,101.3C320,117,400,139,480,133.3C560,128,640,96,720,96C800,96,880,128,960,133.3C1040,139,1120,117,1200,106.7C1280,96,1360,96,1400,96L1440,96L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+                ></path>
+              </svg>
             </div>
-            
           </div>
-
+          <div className="Auth_MobileView_Title">
+            <h1>Welcom Back</h1>
+            <p>Login to your accout</p>
+          </div>
           <form className="Auth_MobileView_Region" onSubmit={handleSubmit}>
-            <div className="Auth_MobileView_Region_Input">
-              <div>
-                <FiUser color="white" size={24} />
+            <div className="Auth_MobileView_Region_LoginArea">
+              <div className="Auth_MobileView_Region_LoginArea_Input">
+                <div>
+                  <FiUser color="#2D642C" size={24} />
+                </div>
+                <input
+                  id="username"
+                  type="text"
+                  placeholder="Tên tài khoản"
+                  value={username}
+                  onChange={handleChangeUsername}
+                />
               </div>
-              <input
-                id="username"
-                type="text"
-                placeholder="Tên tài khoản"
-                value={username}
-                onChange={handleChangeUsername}
-              />
-            </div>
-            <div className="Auth_MobileView_Region_Input">
-              <div>
-                <FiLock color="white" size={24} />
+              <div className="Auth_MobileView_Region_LoginArea_Input">
+                <div>
+                  <FiLock color="#2D642C" size={24} />
+                </div>
+                <input
+                  id="password"
+                  type={open ? "text" : "password"}
+                  placeholder="Mật khẩu"
+                  value={password}
+                  onChange={handleChangePassword}
+                />
+                <div
+                  className="Auth_MobileView_Region_Input_Eye"
+                  onClick={handleOpenEye}
+                >
+                  {open ? (
+                    <FiEye color="#2D642C" size={20} />
+                  ) : (
+                    <FiEyeOff color="#2D642C" size={20} />
+                  )}
+                </div>
               </div>
-              <input
-                id="password"
-                type={open ? "text" : "password"}
-                placeholder="Mật khẩu"
-                value={password}
-                onChange={handleChangePassword}
-              />
-              <div onClick={handleOpenEye}>
-                {open ? <FiEye color="white" /> : <FiEyeOff color="white" />}
+              <div className="Auth_MobileView_Region_LoginArea_FeatureArea">
+                <div className="Auth_MobileView_Region_LoginArea_FeatureArea_Save">
+                  <input type="checkbox" onClick={handleCheckboxClick} />
+                  <div>Lưu đăng nhập</div>
+                </div>
+                <div
+                  className="Auth_MobileView_Region_LoginArea_FeatureArea_ForgetPassword"
+                  onClick={() => navigate("/forgotpassword")}
+                >
+                  Quên mật khẩu?
+                </div>
               </div>
             </div>
-            <div className="Auth_MobileView_Region_Save">
-              <input type="checkbox" onClick={handleCheckboxClick} />
-              <div>Lưu đăng nhập</div>
-            </div>
-            <div className="Auth_MobileView_Region_Button">
-              <button type="submit">Đăng nhập</button>
-            </div>
-            <div className="Auth_MobileView_Region_Stuff">
-              <div onClick={() => navigate("/signup")}>Đăng ký tài khoản</div>
-              <div onClick={() => navigate("/forgotpassword")}>
-                Quên mật khẩu
+
+            <div className="Auth_MobileView_Region_ButtonArea">
+              <div className="Auth_MobileView_Region_ButtonArea_Button">
+                <button type="submit">Đăng nhập</button>
+              </div>
+              <div className="Auth_MobileView_Region_ButtonArea_Stuff">
+                <p className="Auth_MobileView_Region_ButtonArea_Stuff_Text">
+                  Bạn đã có tài khoản?
+                </p>
+                <div
+                  className="Auth_MobileView_Region_ButtonArea_Stuff_BtnSignUp"
+                  onClick={() => navigate("/signup")}
+                >
+                  Đăng ký
+                </div>
               </div>
             </div>
           </form>
