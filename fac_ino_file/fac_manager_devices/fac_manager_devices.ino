79bc6fdb322a9052977785bfa1498eae03b37171
duc <taduc1130@gmail.com>
2024-06-20 10:38:57 +0700

    auto reconnect wifi

--- fac_ino_file/fac_manager_devices/fac_manager_devices.ino
+++ fac_ino_file/fac_manager_devices/fac_manager_devices.ino
@@ -2,13 +2,21 @@
 #include "my_module.h"
 #include "wifiConnection.h"
 
-const String ssid = "DAT_MOBILE";
-const String password = "ktd01042013";
+String ssid = "DAT_MOBILE";
+String password = "ktd01042013";
+WifiCredentials activateAPMode();
 
 void setup() {
   Serial.begin(9600);
+
+  WifiCredentials wifiCreds = activateAPMode();
+  ssid = wifiCreds.ssid;
+  password = wifiCreds.password;
   connectToWiFi(ssid, password);
 }
 
 void loop() {
+  if (!WiFi.isConnected()) {
+    connectToWiFi(ssid, password);
+  }
 }
