426a147104752fd7af3cd293b14a6dd29c93ec0e
Khôi Nguyễn Minh <khoinm0603@gmail.com>
2024-06-20 22:39:46 +0700

    add example dbo.get_equipment_schedule

--- sql/procedure_example.sql
+++ sql/procedure_example.sql
@@ -43,3 +43,16 @@ N'
 
 --EXPECT RESULT state	status
 --				200		success
+
+SELECT * FROM dbo.get_equipment_schedule('BC0002')
+/* EXPECT RESULT
+time_offset		times				_state	_status
+15				03:32:00.0000000	200		success
+15				23:50:00.0000000	200		success
+5				20:12:00.0000000	200		success
+5				14:12:00.0000000	200		success
+15				02:48:00.0000000	200		success
+20				23:59:00.0000000	200		success
+20				00:10:00.0000000	200		success
+20				18:08:00.0000000	200		success
+*/
