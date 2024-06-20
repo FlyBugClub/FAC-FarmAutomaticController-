f0ba718aa3f14791a167b6b69f5b6a701c7ea8bb
duc <taduc1130@gmail.com>
2024-06-20 10:36:21 +0700

    activateAPMode()

--- fac_ino_file/fac_manager_devices/wifiConnection.cpp
+++ fac_ino_file/fac_manager_devices/wifiConnection.cpp
@@ -15,3 +15,25 @@ void connectToWiFi(const String& ssid, const String& password) {
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 }
+
+WifiCredentials activateAPMode() {
+  WifiCredentials credentials;
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Disconnecting from WiFi");
+    WiFi.disconnect();
+  }
+
+  WiFiManager wifiManager;
+
+  if (!wifiManager.autoConnect("ESP8266_AP")) {
+    Serial.println("Failed to connect and hit timeout");
+    ESP.reset();
+    delay(1);
+    return credentials; 
+  } else {
+    credentials.ssid = WiFi.SSID();
+    credentials.password = WiFi.psk();
+    return credentials; 
+  }
+}
--- fac_ino_file/fac_manager_devices/wifiConnection.h
+++ fac_ino_file/fac_manager_devices/wifiConnection.h
@@ -4,6 +4,11 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiManager.h>  
 
+struct WifiCredentials {
+  String ssid;
+  String password;
+};
 void connectToWiFi(const String& ssid, const String& password);
+WifiCredentials activateAPMode();
 
 #endif  // WIFICONNECTION_H
