b483e708bf76a206d03708ab661df741aaeb184a
duc <taduc1130@gmail.com>
2024-06-20 11:54:28 +0700

    del unuse

--- fac_ino_file/fac_manager_devices/wifiConnection.cpp
+++ fac_ino_file/fac_manager_devices/wifiConnection.cpp
@@ -1,7 +1,5 @@
 #include "wifiConnection.h"
 
-
-
 void connectToWiFi(const String& ssid, const String& password) {
   Serial.println("Starting connection to WiFi");
   WiFi.begin(ssid, password);
@@ -10,7 +8,6 @@ void connectToWiFi(const String& ssid, const String& password) {
     delay(100);
     Serial.print(".");
   }
-  // getWhenStart("/api/laststatus/", id_sensor);
   Serial.println("\nWiFi connection successful!");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
