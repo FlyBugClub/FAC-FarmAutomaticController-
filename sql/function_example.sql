238bdae7a79843b27fe83e09bc1619b8363103fd
Khôi Nguyễn Minh <khoinm0603@gmail.com>
2024-06-22 19:42:32 +0700

    adjust example result dbo.get_controlstate

--- sql/function_example.sql
+++ sql/function_example.sql
@@ -1,9 +1,9 @@
 SELECT * FROM [dbo].[get_controlstate]('ESP0001')
 /*  RESULT WILL HAVE THIS FORMAT
-id_equipment	name_equipment	status		_state	_status
-BC0002			PumpB			1			200		success
-BC0104			Pumpr			NULL		201		equipment's status not found
-DHT0505			abc				NULL		201		equipment's status not found
+id_equipment	name_equipment		status	mode	expect_sensor_value		last_status		_state			_status
+BC0002			PumpB				1		0		80						1				200				success
+BC0104			Pumpr				1		0		80						NULL			201				equipment's status not found
+DHT0505			abc					1		0		80						NULL			201				equipment's status not found
 */
 
 
