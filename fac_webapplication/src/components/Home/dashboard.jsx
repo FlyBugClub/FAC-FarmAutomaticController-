0231461779133e879b2512b2d6ca5dd7dbb5cc98
Đinh Quốc Cường <tr6r20@gmail.com>
2024-06-24 19:38:55 +0700

    update search by name and descript

--- fac_webapplication/src/components/Home/dashboard.jsx
+++ fac_webapplication/src/components/Home/dashboard.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState, PureComponent, useContext } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import './home.scss'
 import { BrowserView, MobileView } from "react-device-detect";
 import { MdOutlineLibraryAdd } from "react-icons/md";
 import { MdCircle } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
+import {FiSettings} from "react-icons/fi";
+
 import { callAPi } from "../../services/UserService";
 import Loading from "./loading";
 import { AuthContext } from "../Context/AuthContext";
-import { stringify } from "ajv";
+
 export const Dashboard = ({ weatherState, handleAddDevice }) => {
     const { URL, login, user, farmsct, authDispatch } = useContext(AuthContext);
     const navigate = useNavigate();
     const [farms, setFarms] = useState([]);
-
+    const [totalfarms, setTotalFarms] = useState([]);
     const [loadingState, setLoadingState] = useState(true)
 
     useEffect(() => {
@@ -20,20 +22,8 @@ export const Dashboard = ({ weatherState, handleAddDevice }) => {
     }, [login.status])
 
     useEffect(() => {
-        let farms_temp = [];
-
-        farms.map((item) => {
-            const info = {
-                id_esp: item.id_esp_,
-                name: item.name_esp_
-            };
-            farms_temp = [...farms_temp, info]; 
-        });
-
-        authDispatch({
-            type: "SET_FARM",
-            payload: farms_temp,
-        });
+        
+        
     },[farms])
 
     const getDashboard = async () => {
@@ -42,22 +32,20 @@ export const Dashboard = ({ weatherState, handleAddDevice }) => {
             `${URL}/data/getDashboard/${user.id_user_}`,
         );
         setLoadingState(false)
+        setTotalFarms(res.data)
         setFarms(res.data)
     }
 
     const navigateToFarm = (id_esp) => {
         let farms_temp = [];
-
-        // Sử dụng map() để tạo mảng mới farms_temp
-        farms.map((item, index) => {
+        totalfarms.map((item) => {
             const info = {
                 id_esp: item.id_esp_,
-                name: item.name_esp_
+                name_esp: item.name_esp_,
+                
             };
-            farms_temp = [...farms_temp, info]; // Thêm info vào mảng farms_temp
+            farms_temp = [...farms_temp, info]; 
         });
-
-        // Đảm bảo rằng farms_temp đã chứa dữ liệu sau khi map
 
         authDispatch({
             type: "SET_FARM",
@@ -66,15 +54,36 @@ export const Dashboard = ({ weatherState, handleAddDevice }) => {
         navigate(`/farm/${id_esp}`);
     };
 
+    const handleChange = (event) => {
+        if(farms == undefined) return
+        const newValue = event.target.value;
+
+        let farm_search = []
+        totalfarms.map((item) => {
+            if ((item.name_esp_).toLowerCase().includes(newValue.toLowerCase()) || (item.description_).toLowerCase().includes(newValue.toLowerCase())) {
+                farm_search = [...farm_search, item]
+            };
+        })
+        setFarms(farm_search)
+    }
+
     return (
         <div className="Fac_Home">
             <BrowserView className="Fac_Home_Web" style={weatherState ? { paddingLeft: "15px" } : { paddingLeft: "0px" }} >
                 <div className="Fac_Home_Web_Dashboardcontainer">
                     <div className="Fac_Home_Web_Dashboardcontainer_Header">
                         Farms
-                        <button className="Fac_Home_Web_Dashboardcontainer_Header_Button" onClick={() => { handleAddDevice("farm"); navigate("/addfarm") }}>
-                            <MdOutlineLibraryAdd size={28} style={{ marginRight: "10px" }} />  New farm
-                        </button>
+                        <div className="center">
+                            <input className="Fac_Home_Web_Dashboardcontainer_Header_Input" 
+                            type="text" 
+                            onChange={(e) => {handleChange(e)}}
+                            placeholder="Search...">
+                            </input>
+                            <button className="Fac_Home_Web_Dashboardcontainer_Header_Button" onClick={() => { handleAddDevice("farm"); navigate(`/addfarm/${user.id_user_}`) }}>
+                                <MdOutlineLibraryAdd size={28} style={{ marginRight: "10px" }} />  New farm
+                            </button>
+                        </div>
+                        
                     </div>
                     {
                         loadingState ?
@@ -91,7 +100,14 @@ export const Dashboard = ({ weatherState, handleAddDevice }) => {
                                         {farms.map((item) => (
                                             <div className="Fac_Home_Web_Dashboardcontainer_Farms_Item" key={item.id_esp_} onClick={() => navigateToFarm(item.id_esp_)}>
                                                 <div className="Fac_Home_Web_Dashboardcontainer_Farms_Item_Header">
+                                                    
                                                     {item.name_esp_}
+                                                    
+                                                    <div className="center">
+                                                        <div className="Fac_Home_Web_Dashboardcontainer_Farms_Item_Header_Edit">
+                                                        Setting
+                                                            <FiSettings className="Icon" size={20}  />
+                                                        </div>
                                                     {item.state ? <div className="Fac_Home_Web_Dashboardcontainer_Farms_Item_Header_State">
                                                         <MdCircle size={20} color="#8AFF02" style={{ marginTop: "1px", marginRight: "5px" }} />
                                                         Connected
@@ -101,6 +117,7 @@ export const Dashboard = ({ weatherState, handleAddDevice }) => {
                                                             Disconnected
                                                         </div>
                                                     }
+                                                    </div>
                                                 </div>
                                                 <div className="Fac_Home_Web_Dashboardcontainer_Farms_Item_Description">
                                                     {item.description_}
