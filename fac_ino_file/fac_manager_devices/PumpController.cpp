4f8000d6871e8d0dcf8def0510e8029eaa19efc4
duc <taduc1130@gmail.com>
2024-06-25 14:20:01 +0700

    auto

--- fac_ino_file/fac_manager_devices/PumpController.cpp
+++ fac_ino_file/fac_manager_devices/PumpController.cpp
@@ -4,7 +4,7 @@
 PumpController::PumpController(int pumpPin, MQTTConnection& mqttConnection)
   : _pumpPin(pumpPin), _mqttConnection(mqttConnection) {
   pinMode(_pumpPin, OUTPUT);
-  digitalWrite(_pumpPin, LOW); // Ensure pump starts in the off state
+  digitalWrite(_pumpPin, LOW);  // Ensure pump starts in the off state
 }
 
 void PumpController::handleAction(const char* action, const char* message, int index) {
@@ -12,21 +12,32 @@ void PumpController::handleAction(const char* action, const char* message, int i
     if (strcmp(message, "on") == 0) {
       digitalWrite(_pumpPin, HIGH);
       Serial.println("Pump turned on");
-      publishStatus(action, message, index); // Truyền thêm action
+      publishStatus(action, message, index);  // Truyền thêm action
     } else if (strcmp(message, "off") == 0) {
       digitalWrite(_pumpPin, LOW);
       Serial.println("Pump turned off");
-      publishStatus(action, message, index); // Truyền thêm action
+      publishStatus(action, message, index);  // Truyền thêm action
+    }
+  } else if (strcmp(action, "auto") == 0) {
+    // Xử lý hành động auto ở đây
+    // Ví dụ: Tự động điều khiển theo một số điều kiện nào đó
+    int sensorValue = analogRead(A0);  // Ví dụ đọc giá trị từ cảm biến
+    if (sensorValue > 500) {
+      digitalWrite(_pumpPin, HIGH);  // Bật bơm nếu giá trị cảm biến lớn hơn 500
+      Serial.println("Pump turned on (auto)");
+      publishStatus(action, message, index);  // Truyền action "on" cho publishStatus
+    } else {
+      digitalWrite(_pumpPin, LOW);  // Tắt bơm nếu giá trị cảm biến không lớn hơn 500
+      Serial.println("Pump turned off (auto)");
+      publishStatus(action, message, index);  // Truyền action "off" cho publishStatus
     }
   }
-  
 }
 
 void PumpController::publishStatus(const char* action, const char* message, int index) {
   String indexStr = String(index);
   const char* indexChar = indexStr.c_str();
-  char payload[100]; // Increase array size to accommodate larger payload
-       snprintf(payload, sizeof(payload), "{\"index\": \"%s\", \"payload\": {\"action\": \"%s\", \"messages\": \"%s\"}}", indexChar, action, message);
-    _mqttConnection.publish(_mqttConnection.mqtt_topic_send, payload, true);
-  
+  char payload[100];  // Increase array size to accommodate larger payload
+  snprintf(payload, sizeof(payload), "{\"index\": \"%s\", \"payload\": {\"action\": \"%s\", \"messages\": \"%s\"}}", indexChar, action, message);
+  _mqttConnection.publish(_mqttConnection.mqtt_topic_send, payload, true);
 }
