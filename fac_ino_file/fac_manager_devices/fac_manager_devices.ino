7da5cdd601f3ab89d383e800b6d76382bf7d536c
duc <taduc1130@gmail.com>
2024-06-26 16:09:20 +0700

    send once to client

--- fac_ino_file/fac_manager_devices/fac_manager_devices.ino
+++ fac_ino_file/fac_manager_devices/fac_manager_devices.ino
@@ -25,7 +25,7 @@ const char* char_x_last_will_message = x_last_will_message.c_str();
 
 const int NUM_PUMPS = 4;
 const int pumpPins[NUM_PUMPS] = {2, 12, 13, 15};
-                              // D5, D6, D7, D8
+// D5, D6, D7, D8
 WiFiConnection wifiConn;//                           topic gửi lên        , topic đăng ký/lắng nghe
 MQTTConnection mqttConn(mqtt_server, mqtt_client_id, char_x_send_to_client, char_x_client_to_server, char_x_last_will_message);
 PumpController pumpControllers;
@@ -34,15 +34,15 @@ struct Pump {
   int index;         // Chỉ số của máy bơm
   String action;   // Hành động (action) của máy bơm
   String message;  // Tin nhắn (message) của máy bơm
+  bool isOn;       // Trạng thái của máy bơm
 };
 
 Pump pumps[NUM_PUMPS] = {
-  {1, "", ""},
-  {2, "", ""},
-  {3, "", ""},
-  {4, "", ""} 
+  {1, "", "", false},
+  {2, "", "", false},
+  {3, "", "", false},
+  {4, "", "", false}
 };
-
 
 void setup() {
   Serial.begin(9600);
@@ -55,7 +55,7 @@ void setup() {
 
   mqttConn.setupMQTT();
 
-    for (int i = 0; i < NUM_PUMPS; ++i) {
+  for (int i = 0; i < NUM_PUMPS; ++i) {
     pinMode(pumpPins[i], OUTPUT);
     digitalWrite(pumpPins[i], LOW); // Đặt các chân pin ở mức LOW ban đầu
   }
@@ -68,51 +68,46 @@ void loop() {
   if (!mqttConn.connected()) {
     mqttConn.reconnectMQTT();
   }
-char* payload = nullptr;
-switch (mqttConn.currentIndex) {
+
+  char* payload = nullptr;
+  switch (mqttConn.currentIndex) {
     case 1:
       pumps[0].action = mqttConn.currentAction;
       pumps[0].message = mqttConn.currentMessage;
       payload = pumpControllers.handleAction(pumps[0].action.c_str(), pumps[0].message.c_str(), 1, 2);
-      mqttConn.publish(x_send_to_client, payload)
-      
       break;
     case 2:
       pumps[1].action = mqttConn.currentAction;
       pumps[1].message = mqttConn.currentMessage;
       payload = pumpControllers.handleAction(pumps[1].action.c_str(), pumps[1].message.c_str(), 2, 12);
-       mqttConn.publish(x_send_to_client, payload)
       break;
     case 3:
       pumps[2].action = mqttConn.currentAction;
       pumps[2].message = mqttConn.currentMessage;
       payload = pumpControllers.handleAction(pumps[2].action.c_str(), pumps[2].message.c_str(), 3, 13);
-       mqttConn.publish(x_send_to_client, payload)
       break;
     case 4:
       pumps[3].action = mqttConn.currentAction;
       pumps[3].message = mqttConn.currentMessage;
       payload = pumpControllers.handleAction(pumps[3].action.c_str(), pumps[3].message.c_str(), 4, 15);
-       mqttConn.publish(x_send_to_client, payload)
       break;
     default:
       Serial.println("Index không hợp lệ");
       break;
   }
-if (payload != nullptr) {
+
+  if (payload != nullptr) {
     Serial.println(payload);  // In ra payload
+
+    // Kiểm tra trạng thái hiện tại và trước đó của các máy bơm
+    for (int i = 0; i < NUM_PUMPS; ++i) {
+      bool currentState = digitalRead(pumpPins[i]);
+      if (currentState != pumps[i].isOn) {
+        mqttConn.publish(char_x_send_to_client, payload);  // Gửi lên MQTT nếu trạng thái thay đổi
+        pumps[i].isOn = currentState;  // Cập nhật trạng thái trước đó
+      }
+    }
   }
-
-  // Serial.println("Updated pumps array:");
-  // for (int i = 0; i < NUM_PUMPS; ++i) {
-  //   Serial.print("Pump ");
-  //   Serial.print(pumps[i].index);
-  //   Serial.print(", Action: ");
-  //   Serial.print(pumps[i].action);
-  //   Serial.print(", Message: ");
-  //   Serial.println(pumps[i].message);
-  // }
-
 
   mqttConn.loop();
 }
