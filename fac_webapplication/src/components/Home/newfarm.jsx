d406f75964fe598c70f32c700bfca4dff5f5530e
Đinh Quốc Cường <tr6r20@gmail.com>
2024-06-26 18:03:21 +0700

    update map

--- fac_webapplication/src/components/Home/newfarm.jsx
+++ fac_webapplication/src/components/Home/newfarm.jsx
@@ -7,12 +7,11 @@ import { BsQrCode } from "react-icons/bs";
 import { PiPlusBold } from "react-icons/pi";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { hover } from "@testing-library/user-event/dist/hover";
-import { GoogleMap, LoadScript, Autocomplete } from '@react-google-maps/api';
-import SearchLocationInput from "./locationsearch"
+
 import { AuthContext } from "../Context/AuthContext";
 import QrScanner from 'qr-scanner';
-import MapComponent from "./map";
 import { callAPi } from "../../services/UserService";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
 
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -22,25 +21,77 @@ const AddFarm = ({ weatherState, addDeviceState }) => {
     const { id: paramId } = useParams();
     const [qrcodeState, setQrcodeState] = useState(false)
     const [authQrState, setAuthQrState] = useState(false)
-    const [farmSeleted, setFarmSelected] = useState("")
     const [farmSeletedState, setFarmSelectedState] = useState(false)
     const [qrData, setQrData] = useState([])
     const navigate = useNavigate();
+    const [query, setQuery] = useState("");
+    const autoCompleteRef = useRef(null);
     const fileInputRef = useRef(null);
     const nameEquipmentRef = useRef(null);
+    const nameFarmRef = useRef(null);
+    const DesciptionRef = useRef(null);
     const { URL, farmsct } = useContext(AuthContext);
 
-    const [place, setLocation] = useState("");
+    
+    const googleMapsApiKey = "AIzaSyBF_aYE1ude5Qh1-SEoX1yMVAKz7Z46r7o";
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey,
+        libraries: ["places"]
+    });
     const [selectedLocation, setSelectedLocation] = useState({
         lat: 10.8231,
         lng: 106.6297,
     });
+    
+    let autoComplete;
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+        mapRef.current.setZoom(30);
+    }, []);
+
+    const [farmSeleted, setFarmSelected] = useState("");
 
     useEffect(() => {
-        if (farmsct != undefined) {
-            setFarmSelected(farmsct[0]["name"])
+        if (farmsct.length !== 0) {
+            setFarmSelected(farmsct[0]["name"]);
         }
     }, [farmsct]);
+
+    const handleScriptLoad = (updateQuery, autoCompleteRef) => {
+        autoComplete = new window.google.maps.places.Autocomplete(
+            autoCompleteRef.current,
+            {
+                componentRestrictions: { country: "VN" },
+            }
+        );
+        autoComplete.addListener("place_changed", () => {
+            handlePlaceSelect(updateQuery);
+        });
+
+    };
+
+    if (loadError) return "Error loading maps";
+    if (!isLoaded) return "Loading Maps...";
+
+    const handlePlaceSelect = async (updateQuery) => {
+        const addressObject = await autoComplete.getPlace();
+
+        const query = addressObject.formatted_address;
+        updateQuery(query);
+
+        const latLng = {
+            lat: addressObject?.geometry?.location?.lat(),
+            lng: addressObject?.geometry?.location?.lng(),
+        };
+        setSelectedLocation(latLng);
+    };
+
+    const Focus = () => {
+        handleScriptLoad(setQuery, autoCompleteRef)
+
+    }
+
     const handleScan = (data) => {
         if (data != undefined) {
             const splitResult = data.split(',');
@@ -57,47 +108,66 @@ const AddFarm = ({ weatherState, addDeviceState }) => {
     };
 
     const handleButtonClick = () => {
-        // fileInputRef.current.click();
-        setQrcodeState(true) 
+        fileInputRef.current.click();
     };
 
     const handleAddEquipmentClick = async () => {
-        // console.log(nameEquipmentRef.current.value)
         if (nameEquipmentRef.current.value != "") {
-            console.log(qrData)
-            console.log(paramId)
             let device = [];
             for (let i = 2; i < qrData.length; i++) {
                 if (i == 2) {
-                    device.push({ "id": qrData[i],
-                        "name":nameEquipmentRef.current.value,
-                        "type":"Equipment" })
+                    device.push({
+                        "id": qrData[i],
+                        "name": nameEquipmentRef.current.value,
+                        "type": "Equipment"
+                    })
                 }
                 else {
                     device.push({
                         "id": qrData[i],
-                        "name":"",
-                        "id_equipment":qrData[2],
-                        "type":"Sensor"
+                        "name": "",
+                        "id_equipment": qrData[2],
+                        "type": "Sensor"
                     })
                 }
             }
             let body = {
-                "id_esp" : paramId,
-                "device" : device    
+                "id_esp": paramId,
+                "device": device
             };
-              let res = await callAPi("post", `${URL}/data/insertequipment`,body)
 
-              if (!res.status) {
-                alert (stringify( res))
-              }
-              else navigate(-1)
+            let res = await callAPi("post", `${URL}/data/insertdevice`, body)
+
+            if (!res.status) {
+                toast.error("add equipment fail");
+            }
+            else navigate(-1)
         }
         else toast.error("please enter name equipment");
 
-        // setAuthQrState(true)
-        // navigate('/farm')
     }
+
+    const handleAddFarmClick = async () => {
+        if (DesciptionRef.current.value != "" && nameFarmRef.current.value != "" && autoCompleteRef.current.value != "") {
+            let body = [
+            qrData[2],
+             paramId,
+            nameFarmRef.current.value,
+            DesciptionRef.current.value,
+            selectedLocation.lat,
+            selectedLocation.lng,
+            autoCompleteRef.current.value
+            ];
+            let res = await callAPi("post", `${URL}/data/insertfarm`, body)
+
+            if (!res.status) {
+                toast.error("add equipment fail");
+            }
+            else navigate(-1)
+        }
+        else toast.error("please enter infomation");
+    }
+
 
     const handleFileChange = async (event) => {
         const file = event.target.files[0];
@@ -109,16 +179,7 @@ const AddFarm = ({ weatherState, addDeviceState }) => {
             console.error('Error scanning QR code:', error);
         }
     };
-    const handleFocus = () => {
-        if (place != "") {
-            const { geometry } = place;
-            const latLng = {
-                lat: geometry.location.lat(),
-                lng: geometry.location.lng()
-            };
-            setSelectedLocation(latLng);
-        }
-    };
+
     return (
         <div className="Fac_Home">
             <BrowserView className="Fac_Home_Web" style={weatherState ? { paddingLeft: "15px" } : { paddingLeft: "0px" }}>
@@ -135,26 +196,43 @@ const AddFarm = ({ weatherState, addDeviceState }) => {
                                 <div className="Fac_Home_Web_Addfarmcontainer_Body_Left">
                                     <div className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items">
                                         Name:
-                                        <textarea className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items_Input" maxLength="30"></textarea>
+                                        <input className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items_Input" ref={nameFarmRef} maxLength="30" ></input>
                                     </div>
                                     <div className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items" style={{ marginTop: "15px" }} maxLength="30">
                                         Address:
-                                        <textarea
-                                            className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items_Input"
-                                            onChange={(e) => setLocation(e.target.value)}
-                                            value={place}>
-
-                                        </textarea>
+                                        <input className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items_Input"
+                                            maxLength="30"
+                                            placeholder="Search Places ..."
+                                            ref={autoCompleteRef}
+                                            onFocus={() => Focus()}
+                                        ></input>
                                     </div>
                                     <div className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items" style={{ marginTop: "15px" }} maxLength="150">
                                         Description:
-                                        <textarea className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items_Input" style={{ height: "100px", textAlign: "left" }} onFocus={handleFocus}></textarea>
+                                        <textarea
+                                            className="Fac_Home_Web_Addfarmcontainer_Body_Left_Items_Input"
+                                            ref={DesciptionRef}
+                                            style={{ height: "100px", textAlign: "left" }}
+                                        ></textarea>
                                     </div>
                                 </div>
                                 <div className="Fac_Home_Web_Addfarmcontainer_Body_Right">
 
                                     <div className="Fac_Home_Web_Addfarmcontainer_Body_Right_Map" >
-                                        <MapComponent selectedLocation={selectedLocation} />
+                                        <GoogleMap
+                                            mapContainerStyle={{
+                                                height: "320px",
+                                            }}
+                                            center={selectedLocation}
+                                            zoom={13}
+                                            onLoad={onMapLoad}
+
+                                        >
+                                            <MarkerF
+                                                position={selectedLocation}
+                                                icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+                                            />
+                                        </GoogleMap>
                                     </div>
 
                                     <div className="Fac_Home_Web_Addfarmcontainer_Body_Right_Buttons">
@@ -162,7 +240,7 @@ const AddFarm = ({ weatherState, addDeviceState }) => {
                                             <BsQrCode size={20} style={{ marginRight: "10px" }} />
                                             Add QR code again
                                         </button>
-                                        <button className="Fac_Home_Web_Addfarmcontainer_Body_Right_Buttons_Items" onClick={() => navigate('/dashboard')}>
+                                        <button className="Fac_Home_Web_Addfarmcontainer_Body_Right_Buttons_Items" onClick={() => handleAddFarmClick()}>
                                             <AiOutlinePlusCircle size={20} style={{ marginRight: "10px" }} />
                                             Add farm
                                         </button>
@@ -225,7 +303,7 @@ const AddFarm = ({ weatherState, addDeviceState }) => {
                                     <button className="Fac_Home_Web_Addfarmcontainer_Body_Qrbutton" style={authQrState ? { outline: " 2px solid rgba(255, 0, 0, 0.9)" } : { outline: " 2px solid rgba(255, 255, 255, 0.9)" }} onClick={() => handleButtonClick()}>
                                         <BsQrCode size={26} style={{ marginRight: "10px" }} />
                                         Add Qr code file
-                                    </button>
+                                    </button>   
                                     <input
                                         type="file"
                                         ref={fileInputRef}
