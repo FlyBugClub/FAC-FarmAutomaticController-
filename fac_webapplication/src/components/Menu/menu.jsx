cd1e3a165bca1a78bc4ee369187206a8876ef548
Đinh Quốc Cường <tr6r20@gmail.com>
2024-07-08 19:20:11 +0700

    WIP on cuong15/6

--- fac_webapplication/src/components/Menu/menu.jsx
+++ fac_webapplication/src/components/Menu/menu.jsx
@@ -10,151 +10,88 @@ import { FaRegAddressBook } from "react-icons/fa6";
 import { AuthContext } from "../Context/AuthContext";
 const Menu = ({ handleWeather, weatherState }) => {
   const { URL, login, user, authDispatch } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const [menuState, setMenuState] = useState("dashboard");
-  const [userState, setUserState] = useState("");
-
-  const handleLogout = () => {
-    authDispatch({
-      type: "SET_LOGIN",
-      payload: { status: false, isSave: "" },
-    });
-    console.log(login.status);
-    localStorage.clear();
-    sessionStorage.clear();
-    navigate("/");
-  };
-  const handleMenu = (key) => {
-    if (key === userState) {
-      setUserState("");
-    } else {
-      setUserState(key);
+    const navigate = useNavigate();
+    const [menuState, setMenuState] = useState("dashboard")
+    const [userState, setUserState] = useState("")
+    const [adminState,setAdminState] = useState(false)
+ 
+    const handleLogout = () => {
+        authDispatch({
+            type: "SET_LOGIN",
+            payload: { status: false, isSave: '' },
+          })
+        localStorage.clear();
+        sessionStorage.clear();
+        navigate("/");
     }
-    if (key !== "user") {
-      setMenuState(key);
+    const handleMenu = (key) => {
+        if (key === userState) {setUserState("");}
+        else {setUserState(key);}
+        if (key !== "user") {setMenuState(key);}
     }
-  };
 
   return (
     <div className="Fac_Menu">
-      <BrowserView className="Fac_Menu_Web">
-        <div className="Fac_Menu_Web_Container">
-          <img
-            className="Fac_Menu_Web_Container_Logo"
-            src="/icons/Bug(Trắng).png"
-            alt=""
-          />
-          <div className="Fac_Menu_Web_Container_Menu">
-            <div
-              className="Fac_Menu_Web_Container_Menu_Elements"
-              onClick={() => handleWeather()}
-              style={
-                weatherState
-                  ? {
-                      backgroundColor: "rgba(200, 200, 200, 0.3)",
-                      borderRadius: "50%",
-                    }
-                  : {}
-              }
-            >
-              <WiDayCloudy
-                className="Fac_Menu_Web_Container_Menu_Elements_Icon"
-                size={35}
-              />
-            </div>
-            <div
-              className="Fac_Menu_Web_Container_Menu_Elements"
-              onClick={() => (handleMenu("dashboard"), navigate("/dashboard"))}
-              style={
-                menuState === "dashboard"
-                  ? {
-                      backgroundColor: "rgba(200, 200, 200, 0.3)",
-                      borderRadius: "50%",
-                    }
-                  : {}
-              }
-            >
-              <FiGrid
-                className="Fac_Menu_Web_Container_Menu_Elements_Icon"
-                size={25}
-              />
-            </div>
-            <div
-              className="Fac_Menu_Web_Container_Menu_Elements"
-              onClick={() => handleMenu("Notification")}
-              style={
-                menuState === "Notification"
-                  ? {
-                      backgroundColor: "rgba(200, 200, 200, 0.3)",
-                      borderRadius: "50%",
-                    }
-                  : {}
-              }
-            >
-              <FiSettings
-                className="Fac_Menu_Web_Container_Menu_Elements_Icon"
-                size={25}
-              />
-            </div>
-            <div
-              className="Fac_Menu_Web_Container_Menu_Elements"
-              onClick={() => handleMenu("Settings")}
-              style={
-                menuState === "Settings"
-                  ? {
-                      backgroundColor: "rgba(200, 200, 200, 0.3)",
-                      borderRadius: "50%",
-                    }
-                  : {}
-              }
-            >
-              <FiBell
-                className="Fac_Menu_Web_Container_Menu_Elements_Icon"
-                size={25}
-              />
-            </div>
-          </div>
-          <div style={{ position: "relative", zIndex: "10" }}>
-            <img
-              className="Fac_Menu_Web_Container_Avartar"
-              src="/icons/user.png"
-              alt=""
-              onClick={() => handleMenu("user")}
-            />
-            {userState === "user" ? (
-              <div className="Fac_Menu_Web_Container_Dropbox">
-                <div className="Fac_Menu_Web_Container_Dropbox_Header">
-                  <div className="Fac_Menu_Web_Container_Dropbox_Header_Info">
-                    {user.user_name_}
-                    <div style={{ fontSize: "12px", fontWeight: "400" }}>
-                      {user.gmail_}
+        <BrowserView className="Fac_Menu_Web">
+                <div className="Fac_Menu_Web_Container">
+                    <img className="Fac_Menu_Web_Container_Logo" src="/icons/Bug(Trắng).png" alt="" />
+                    <div className="Fac_Menu_Web_Container_Menu">
+                        {
+                            !adminState &&  
+                            <div className="Fac_Menu_Web_Container_Menu_Elements" onClick={() => handleWeather(false)} style={weatherState ? { backgroundColor: "rgba(200, 200, 200, 0.3)", borderRadius: "50%" } : {}}>
+                            <WiDayCloudy className="Fac_Menu_Web_Container_Menu_Elements_Icon" size={35} />
+
+                        </div>
+                        }
+                        
+                        <div className="Fac_Menu_Web_Container_Menu_Elements" onClick={() => {handleMenu("dashboard");setAdminState(false); navigate("/dashboard") }} style={menuState === "dashboard" ? { backgroundColor: "rgba(200, 200, 200, 0.3)", borderRadius: "50%" } : {}}>
+                            <FiGrid className="Fac_Menu_Web_Container_Menu_Elements_Icon" size={25} />
+
+                        </div>
+                        <div className="Fac_Menu_Web_Container_Menu_Elements" onClick={() => handleMenu("Notification")} style={menuState === "Notification" ? { backgroundColor: "rgba(200, 200, 200, 0.3)", borderRadius: "50%" } : {}}>
+                            <FiSettings className="Fac_Menu_Web_Container_Menu_Elements_Icon" size={25} />
+
+                        </div>
+                        <div className="Fac_Menu_Web_Container_Menu_Elements" onClick={() => handleMenu("Settings")} style={menuState === "Settings" ? { backgroundColor: "rgba(200, 200, 200, 0.3)", borderRadius: "50%" } : {}}>
+                            <FiBell className="Fac_Menu_Web_Container_Menu_Elements_Icon" size={25} />
+
+                        </div>
+                        {
+                            user["membership_"]  == "admin"&&  
+                            <div className="Fac_Menu_Web_Container_Menu_Elements" onClick={() => { handleWeather(true);setAdminState(true);handleMenu("Manage");navigate("/admin")}} style={menuState === "Manage" ? { backgroundColor: "rgba(200, 200, 200, 0.3)", borderRadius: "50%" } : {}}>
+                            <FaRegAddressBook className="Fac_Menu_Web_Container_Menu_Elements_Icon" size={25} />
+                        </div>
+                        }
+
                     </div>
-                  </div>
+                    <div style={{ position: "relative", zIndex: "10" }}>
+                        <img className="Fac_Menu_Web_Container_Avartar" src="/icons/user.png" alt="" onClick={() => handleMenu("user")} />
+                        {userState === "user" ?
+                            <div className="Fac_Menu_Web_Container_Dropbox" >
+                                <div className="Fac_Menu_Web_Container_Dropbox_Header">
+                                    <div className="Fac_Menu_Web_Container_Dropbox_Header_Info">
+                                        {user.user_name_}
+                                        <div style={{ fontSize: "12px", fontWeight: "400" }}>{user.gmail_}</div>
+                                    </div>
+
+                                </div>
+                                <div className="Fac_Menu_Web_Container_Dropbox_Options" onClick={() => { navigate("/usersetting"); setUserState("") }}>
+                                    <FiSettings style={{ marginRight: "10px" }} />
+                                    Account
+                                </div>
+                                <div onClick={() => handleLogout()} className="Fac_Menu_Web_Container_Dropbox_Options">
+                                    <FiUserMinus style={{ marginRight: "10px" }} />
+                                    Log out
+                                </div>
+
+                            </div> :
+                            <></>
+                        }
+
+                    </div>
+
                 </div>
-                <div
-                  className="Fac_Menu_Web_Container_Dropbox_Options"
-                  onClick={() => {
-                    navigate("/usersetting");
-                    setUserState("");
-                  }}
-                >
-                  <FiSettings style={{ marginRight: "10px" }} />
-                  Account
-                </div>
-                <div
-                  onClick={() => handleLogout()}
-                  className="Fac_Menu_Web_Container_Dropbox_Options"
-                >
-                  <FiUserMinus style={{ marginRight: "10px" }} />
-                  Log out
-                </div>
-              </div>
-            ) : (
-              <></>
-            )}
-          </div>
-        </div>
-      </BrowserView>
+            </BrowserView>
 
       <MobileView className="Fac_Menu_Mobile">
         <div className="Fac_Menu_Mobile_Container">
